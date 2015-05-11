f17974ebe739e6f446cbc37f6d21c28affbe7580
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-05-11 14:50:14 +0530

    fixing error on empty result values

--- app/controllers/aml_search_controller.rb
+++ app/controllers/aml_search_controller.rb
@@ -14,8 +14,8 @@ class AmlSearchController < ApplicationController
       if params[:search_params][:firstName].present?
         @search_params = search_params(params[:search_params])
         results = get_response_from_api(CONFIG[:aml_search_url] + @search_params) rescue []
-        @results_count = results.count
-        @results = results.paginate(:per_page => 10, :page => params[:page])
+        @results_count = results.count rescue 0
+        @results = results.paginate(:per_page => 10, :page => params[:page]) unless results.nil?
       else
         flash[:alert] = "First Name should not be empty"
       end
