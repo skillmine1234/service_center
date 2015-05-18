ce75123d01b6f648906b1837ce95420658607912
divyajayan <divya.jayan@quantiguous.com>
2015-05-18 17:15:12 +0530

    INRW-84-Partner Validation

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -12,4 +12,6 @@ class Partner < ActiveRecord::Base
   validates_presence_of :code, :name, :account_no, :txn_hold_period_days
   validates_uniqueness_of :code
   validates :low_balance_alert_at, :numericality => { :greater_than_or_equal_to => 0, :allow_nil => true }
+  validates :account_no, :numericality => {:only_integer => true}, length: {in: 10..16}
+  validates :account_ifsc, format: {with: /[A-Z|a-z]{4}[0][A-Za-z0-9]{6}$/ }
 end
