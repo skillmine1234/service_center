27ad334532f929e4c9761626f809502da997da4f
divyajayan <divya.jayan@quantiguous.com>
2015-05-19 18:16:53 +0530

    changes on commits INRW-64 & INRW-84

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -7,5 +7,6 @@ class Partner < ActiveRecord::Base
   validates_uniqueness_of :code
   validates :low_balance_alert_at, :numericality => { :greater_than_or_equal_to => 0, :allow_nil => true }
   validates :account_no, :numericality => {:only_integer => true}, length: {in: 10..16}
-  validates :account_ifsc, format: {with: /[A-Z|a-z]{4}[0][A-Za-z0-9]{6}$/ }
+  validates :account_ifsc, format: {with: /\A[A-Z|a-z]{4}[0][A-Za-z0-9]{6}+\z/ }
+  validates :txn_hold_period_days, :numericality => { :greater_than => 0}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150518082947) do
+ActiveRecord::Schema.define(version: 20150518114920) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "resource_id",   null: false
@@ -114,20 +113,10 @@ ActiveRecord::Schema.define(version: 20150518082947) do
     t.datetime "updated_at",   null: false
   end
 
-
-  create_table "banks", :force => true do |t|
-    t.string   "ifsc"
-    t.string   "name"
-    t.boolean  "imps_enabled"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-  end
-
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
     t.text     "handler",                null: false
-
     t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
@@ -229,18 +218,18 @@ ActiveRecord::Schema.define(version: 20150518082947) do
     t.integer  "txn_hold_period_days",                 default: 7, null: false
     t.string   "identity_user_id",          limit: 20
     t.float    "low_balance_alert_at"
-    t.string   "remitter_sms_allowed",      :limit => 1
-    t.string   "remitter_email_allowed",    :limit => 1
-    t.string   "beneficiary_sms_allowed",   :limit => 1
-    t.string   "beneficiary_email_allowed", :limit => 1
-    t.string   "allow_neft",                :limit => 1
-    t.string   "allow_rtgs",                :limit => 1
-    t.string   "allow_imps",                :limit => 1
-    t.string   "created_by",                :limit => 20
-    t.string   "updated_by",                :limit => 20
-    t.datetime "created_at",                                             :null => false
-    t.datetime "updated_at",                                             :null => false
-    t.integer  "lock_version"
+    t.string   "remitter_sms_allowed",      limit: 1
+    t.string   "remitter_email_allowed",    limit: 1
+    t.string   "beneficiary_sms_allowed",   limit: 1
+    t.string   "beneficiary_email_allowed", limit: 1
+    t.string   "allow_neft",                limit: 1
+    t.string   "allow_rtgs",                limit: 1
+    t.string   "allow_imps",                limit: 1
+    t.string   "created_by",                limit: 20
+    t.string   "updated_by",                limit: 20
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "lock_version",                         default: 0, null: false
   end
 
   create_table "purpose_codes", force: :cascade do |t|
