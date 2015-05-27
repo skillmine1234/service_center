ebefc4d82c2f08bd7dc9daa105455378671000dd
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-05-27 16:14:06 +0530

    fixing migrations

--- db/migrate/20150527062043_remove_columns_from_inward_remittance_locks.rb
+++ db/migrate/20150527062043_remove_columns_from_inward_remittance_locks.rb
@@ -1,6 +1,6 @@
 class RemoveColumnsFromInwardRemittanceLocks < ActiveRecord::Migration
   def change
     remove_column :inward_remittances_locks, :req_no
-    remove_column :inward_remittances_locks, :parter_code
+    remove_column :inward_remittances_locks, :partner_code
   end
 end
