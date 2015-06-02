d64e993ad3f719e2d4f540366a2a97f86c229f4f
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-06-02 19:06:50 +0530

    fixing advanced search inward remittance

--- app/helpers/inward_remittance_helper.rb
+++ app/helpers/inward_remittance_helper.rb
@@ -6,7 +6,7 @@ module InwardRemittanceHelper
     inw_remittances = inw_remittances.where("lower(partner_code)=?",params[:partner_code].downcase) if params[:partner_code].present?
     inw_remittances = inw_remittances.where("req_transfer_type=?",params[:req_transfer_type]) if params[:req_transfer_type].present?
     inw_remittances = inw_remittances.where("transfer_type=?",params[:transfer_type]) if params[:transfer_type].present?
-    inw_remittances = inw_remittances.where("transfer_ccy>=? and transfer_ccy <=?",params[:from_amount].to_f,params[:to_amount].to_f) if params[:to_amount].present? and params[:from_amount].present?
+    inw_remittances = inw_remittances.where("transfer_amount>=? and transfer_amount <=?",params[:from_amount].to_f,params[:to_amount].to_f) if params[:to_amount].present? and params[:from_amount].present?
     inw_remittances = inw_remittances.where("req_timestamp>=? and req_timestamp<=?",Time.zone.parse(params[:from_date]).beginning_of_day,Time.zone.parse(params[:to_date]).end_of_day) if params[:from_date].present? and params[:to_date].present?
     inw_remittances
   end
