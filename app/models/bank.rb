5ccdd1384bb29cd499070b474289fe28fc7542d5
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-06-03 13:56:33 +0530

    INRW-148 - changes in bank screen

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,7 +1,7 @@
 class Bank < ActiveRecord::Base
   audited
   validates_presence_of :ifsc, :name
-  validates :ifsc, format: {with: /\A[A-Z|a-z]{4}[0][A-Za-z0-9]{6}+\z/ }
+  validates :ifsc, format: {with: /\A[A-Z|a-z]{4}[0][A-Za-z0-9]{6}+\z/, :allow_blank => true }
   validates_inclusion_of :imps_enabled, :in => [true, false]
 
   belongs_to :created_user, :foreign_key =>'created_by', :class_name => 'User'
