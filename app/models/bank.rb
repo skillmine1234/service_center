95ef977375a47edf64498a8504a26f43a90e7b0f
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-06-10 17:25:00 +0530

    INRW-173 - bank changes

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,10 +1,8 @@
 class Bank < ActiveRecord::Base
   audited
   validates_presence_of :ifsc, :name
-  validates :ifsc, format: {with: /\A[A-Z|a-z]{4}[0][A-Za-z0-9]{6}+\z/, :allow_blank => true, message: "invalid format - expected format is :
-  {[A-Z|a-z]{4}[0][A-Za-z0-9]{6}}" }
-  validates :name, format: {with: /\A[A-Za-z\s]+\z/, message: "invalid format - expected format is :
-  {[A-Za-z\s]}"}
+  validates :ifsc, format: {with: /\A[A-Z|a-z]{4}[0][A-Za-z0-9]{6}+\z/, :allow_blank => true, message: "invalid format - expected format is : {[A-Z|a-z]{4}[0][A-Za-z0-9]{6}}" }
+  validates :name, format: {with: /\A[A-Za-z\s]+\z/, message: "invalid format - expected format is : {[A-Za-z\s]}"}
   belongs_to :created_user, :foreign_key =>'created_by', :class_name => 'User'
   belongs_to :updated_user, :foreign_key =>'updated_by', :class_name => 'User'
 
