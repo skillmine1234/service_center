5392f2498095dc5b81e749b10f4ded6daaf13f9f
divyajayan <divya.jayan@quantiguous.com>
2015-06-11 15:16:02 +0530

    migrations

---
+++ db/migrate/20150611060417_create_ecol_pending_validations.rb
@@ -0,0 +1,9 @@
+class CreateEcolPendingValidations < ActiveRecord::Migration
+  def change
+    create_table :ecol_pending_validations do |t|
+      t.string :broker_uuid, :limit => 500
+      t.references :ecol_transaction
+      t.datetime :created_at
+    end
+  end
+end
