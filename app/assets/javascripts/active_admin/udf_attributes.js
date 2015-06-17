c4845ce5f98a21cabcdb106ed3e7375ccb8ff534
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-06-17 14:41:39 +0530

    INRW-187 - udf_attribute changes

--- app/assets/javascripts/active_admin/udf_attributes.js
+++ app/assets/javascripts/active_admin/udf_attributes.js
@@ -11,12 +11,12 @@ $(document).ready(function(){
           $('#udf_attribute_max_value').val('');
           $('#udf_attribute_select_options').val('');
           $('#udf_attribute_data_type').prop('disabled',false);
-          $('#udf_attribute_length').prop('disabled',true);
-          $('#udf_attribute_min_length').prop('disabled',true);
-          $('#udf_attribute_max_length').prop('disabled',true);
-          $('#udf_attribute_min_value').prop('disabled',true);
-          $('#udf_attribute_max_value').prop('disabled',true);
-          $('#udf_attribute_select_option').prop('disabled',true);
+          $('#udf_attribute_length').prop('readOnly',true);
+          $('#udf_attribute_min_length').prop('readOnly',true);
+          $('#udf_attribute_max_length').prop('readOnly',true);
+          $('#udf_attribute_min_value').prop('readOnly',true);
+          $('#udf_attribute_max_value').prop('readOnly',true);
+          $('#udf_attribute_select_option').prop('readOnly',true);
         }
         else if(controlType == "DropDown"){
           $('#udf_attribute_data_type').val('');
@@ -26,12 +26,12 @@ $(document).ready(function(){
           $('#udf_attribute_min_value').val('');
           $('#udf_attribute_max_value').val('');
           $('#udf_attribute_data_type').prop('disabled',true);
-          $('#udf_attribute_length').prop('disabled',true);
-          $('#udf_attribute_min_length').prop('disabled',true);
-          $('#udf_attribute_max_length').prop('disabled',true);
-          $('#udf_attribute_min_value').prop('disabled',true);
-          $('#udf_attribute_max_value').prop('disabled',true);
-          $('#udf_attribute_select_options').prop('disabled',false);
+          $('#udf_attribute_length').prop('readOnly',true);
+          $('#udf_attribute_min_length').prop('readOnly',true);
+          $('#udf_attribute_max_length').prop('readOnly',true);
+          $('#udf_attribute_min_value').prop('readOnly',true);
+          $('#udf_attribute_max_value').prop('readOnly',true);
+          $('#udf_attribute_select_options').prop('readOnly',false);
         }
         else{
           $('#udf_attribute_data_type').val('');
@@ -42,12 +42,12 @@ $(document).ready(function(){
           $('#udf_attribute_max_value').val('');
           $('#udf_attribute_select_options').val('');
           $('#udf_attribute_data_type').prop('disabled',true);
-          $('#udf_attribute_length').prop('disabled',true);
-          $('#udf_attribute_min_length').prop('disabled',true);
-          $('#udf_attribute_max_length').prop('disabled',true);
-          $('#udf_attribute_min_value').prop('disabled',true);
-          $('#udf_attribute_max_value').prop('disabled',true);
-          $('#udf_attribute_select_options').prop('disabled',true);
+          $('#udf_attribute_length').prop('readOnly',true);
+          $('#udf_attribute_min_length').prop('readOnly',true);
+          $('#udf_attribute_max_length').prop('readOnly',true);
+          $('#udf_attribute_min_value').prop('readOnly',true);
+          $('#udf_attribute_max_value').prop('readOnly',true);
+          $('#udf_attribute_select_options').prop('readOnly',true);
         }
     });
 
@@ -55,35 +55,35 @@ $(document).ready(function(){
     $('#udf_attribute_data_type').on("change",function(){
         var dataType = $(this).val();
         if(dataType == "String"){
-          $('#udf_attribute_length').prop('disabled',false);
-          $('#udf_attribute_min_length').prop('disabled',false);
-          $('#udf_attribute_max_length').prop('disabled',false);
+          $('#udf_attribute_length').prop('readOnly',false);
+          $('#udf_attribute_min_length').prop('readOnly',false);
+          $('#udf_attribute_max_length').prop('readOnly',false);
           $('#udf_attribute_min_value').val('');
           $('#udf_attribute_max_value').val('');
           $('#udf_attribute_select_options').val('');
-          $('#udf_attribute_min_value').prop('disabled',true);
-          $('#udf_attribute_max_value').prop('disabled',true);
-          $('#udf_attribute_select_options').prop('disabled',true);
+          $('#udf_attribute_min_value').prop('readOnly',true);
+          $('#udf_attribute_max_value').prop('readOnly',true);
+          $('#udf_attribute_select_options').prop('readOnly',true);
         }
         else if(dataType == "Numeric"){
-          $('#udf_attribute_length').prop('disabled',true);
-          $('#udf_attribute_min_length').prop('disabled',true);
-          $('#udf_attribute_max_length').prop('disabled',true);
+          $('#udf_attribute_length').prop('readOnly',true);
+          $('#udf_attribute_min_length').prop('readOnly',true);
+          $('#udf_attribute_max_length').prop('readOnly',true);
           $('#udf_attribute_length').val('');
           $('#udf_attribute_min_length').val('');
           $('#udf_attribute_max_length').val('');
           $('#udf_attribute_select_options').val('');
-          $('#udf_attribute_min_value').prop('disabled',false);
-          $('#udf_attribute_max_value').prop('disabled',false);
-          $('#udf_attribute_select_options').prop('disabled',true);
+          $('#udf_attribute_min_value').prop('readOnly',false);
+          $('#udf_attribute_max_value').prop('readOnly',false);
+          $('#udf_attribute_select_options').prop('readOnly',true);
         }
         else{
-          $('#udf_attribute_length').prop('disabled',true);
-          $('#udf_attribute_min_length').prop('disabled',true);
-          $('#udf_attribute_max_length').prop('disabled',true);
-          $('#udf_attribute_min_value').prop('disabled',true);
-          $('#udf_attribute_max_value').prop('disabled',true);
-          $('#udf_attribute_select_options').prop('disabled',true);
+          $('#udf_attribute_length').prop('readOnly',true);
+          $('#udf_attribute_min_length').prop('readOnly',true);
+          $('#udf_attribute_max_length').prop('readOnly',true);
+          $('#udf_attribute_min_value').prop('readOnly',true);
+          $('#udf_attribute_max_value').prop('readOnly',true);
+          $('#udf_attribute_select_options').prop('readOnly',true);
           $('#udf_attribute_length').val('');
           $('#udf_attribute_min_length').val('');
           $('#udf_attribute_max_length').val('');
@@ -96,52 +96,52 @@ $(document).ready(function(){
 
     if($('#udf_attribute_control_type').val() == 'TextBox'){
       $('#udf_attribute_data_type').prop('disabled',false);
-      $('#udf_attribute_length').prop('disabled',true);
-      $('#udf_attribute_min_length').prop('disabled',true);
-      $('#udf_attribute_max_length').prop('disabled',true);
-      $('#udf_attribute_min_value').prop('disabled',true);
-      $('#udf_attribute_max_value').prop('disabled',true);
-      $('#udf_attribute_select_options').prop('disabled',true);
+      $('#udf_attribute_length').prop('readOnly',true);
+      $('#udf_attribute_min_length').prop('readOnly',true);
+      $('#udf_attribute_max_length').prop('readOnly',true);
+      $('#udf_attribute_min_value').prop('readOnly',true);
+      $('#udf_attribute_max_value').prop('readOnly',true);
+      $('#udf_attribute_select_options').prop('readOnly',true);
     }
     else if($('#udf_attribute_control_type').val() == "DropDown"){
       $('#udf_attribute_data_type').prop('disabled',true);
-      $('#udf_attribute_length').prop('disabled',true);
-      $('#udf_attribute_min_length').prop('disabled',true);
-      $('#udf_attribute_max_length').prop('disabled',true);
-      $('#udf_attribute_min_value').prop('disabled',true);
-      $('#udf_attribute_max_value').prop('disabled',true);
-      $('#udf_attribute_select_options').prop('disabled',false);
+      $('#udf_attribute_length').prop('readOnly',true);
+      $('#udf_attribute_min_length').prop('readOnly',true);
+      $('#udf_attribute_max_length').prop('readOnly',true);
+      $('#udf_attribute_min_value').prop('readOnly',true);
+      $('#udf_attribute_max_value').prop('readOnly',true);
+      $('#udf_attribute_select_options').prop('readOnly',false);
     }
     else{
       $('#udf_attribute_data_type').prop('disabled',true);
-      $('#udf_attribute_length').prop('disabled',true);
-      $('#udf_attribute_min_length').prop('disabled',true);
-      $('#udf_attribute_max_length').prop('disabled',true);
-      $('#udf_attribute_min_value').prop('disabled',true);
-      $('#udf_attribute_max_value').prop('disabled',true);
-      $('#udf_attribute_select_options').prop('disabled',true);
+      $('#udf_attribute_length').prop('readOnly',true);
+      $('#udf_attribute_min_length').prop('readOnly',true);
+      $('#udf_attribute_max_length').prop('readOnly',true);
+      $('#udf_attribute_min_value').prop('readOnly',true);
+      $('#udf_attribute_max_value').prop('readOnly',true);
+      $('#udf_attribute_select_options').prop('readOnly',true);
     };
 
     if($('#udf_attribute_data_type').val() == "String"){
-      $('#udf_attribute_length').prop('disabled',false);
-      $('#udf_attribute_min_length').prop('disabled',false);
-      $('#udf_attribute_max_length').prop('disabled',false);
-      $('#udf_attribute_min_value').prop('disabled',true);
-      $('#udf_attribute_max_value').prop('disabled',true);
+      $('#udf_attribute_length').prop('readOnly',false);
+      $('#udf_attribute_min_length').prop('readOnly',false);
+      $('#udf_attribute_max_length').prop('readOnly',false);
+      $('#udf_attribute_min_value').prop('readOnly',true);
+      $('#udf_attribute_max_value').prop('readOnly',true);
     }
     else if($('#udf_attribute_data_type').val() == "Numeric"){
-      $('#udf_attribute_length').prop('disabled',true);
-      $('#udf_attribute_min_length').prop('disabled',true);
-      $('#udf_attribute_max_length').prop('disabled',true);
-      $('#udf_attribute_min_value').prop('disabled',false);
-      $('#udf_attribute_max_value').prop('disabled',false);
+      $('#udf_attribute_length').prop('readOnly',true);
+      $('#udf_attribute_min_length').prop('readOnly',true);
+      $('#udf_attribute_max_length').prop('readOnly',true);
+      $('#udf_attribute_min_value').prop('readOnly',false);
+      $('#udf_attribute_max_value').prop('readOnly',false);
     }
     else{
-      $('#udf_attribute_length').prop('disabled',true);
-      $('#udf_attribute_min_length').prop('disabled',true);
-      $('#udf_attribute_max_length').prop('disabled',true);
-      $('#udf_attribute_min_value').prop('disabled',true);
-      $('#udf_attribute_max_value').prop('disabled',true);
+      $('#udf_attribute_length').prop('readOnly',true);
+      $('#udf_attribute_min_length').prop('readOnly',true);
+      $('#udf_attribute_max_length').prop('readOnly',true);
+      $('#udf_attribute_min_value').prop('readOnly',true);
+      $('#udf_attribute_max_value').prop('readOnly',true);
     };
 
   $('#udf_attribute_form').on('submit', function(){
