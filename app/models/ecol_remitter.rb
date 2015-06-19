a692b0cb2d713f2898c1babc79e51a60a61b3295
divyajayan <divya.jayan@quantiguous.com>
2015-06-19 16:40:27 +0530

    INRW-211 Additional Changes in EcolRemitter

--- app/models/ecol_remitter.rb
+++ app/models/ecol_remitter.rb
@@ -10,16 +10,15 @@ class EcolRemitter < ActiveRecord::Base
   belongs_to :ecol_customer
   
   validates :customer_code, presence: true, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}'}, length: {maximum: 15, minimum: 1}
-  validates :remitter_code, presence: true, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 15, minimum: 1}
-  validates :credit_acct_no, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 25, minimum: 1}, :allow_blank =>true
-  validates :customer_subcode_email, format: {with: /\A[a-z|A-Z|0-9|\@|\.]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9|@|.]}'}, length: {maximum: 100, minimum: 3}, :allow_blank =>true
+  validates :customer_subcode, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}'}, length: {maximum: 15, minimum: 1}, :allow_blank =>true
+  validates :remitter_code, presence: true, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 28, minimum: 1}
+  validates :credit_acct_no, format: {with: /\A[0-9]+\z/, :message => 'Invalid format, expected format is : {[0-9]}' }, length: {maximum: 15, minimum: 1}, :allow_blank =>true
   validates :customer_subcode_mobile, format: {with: /\A[0-9]+\z/, :message => 'Invalid format, expected format is : {[0-9]}' }, length: {maximum: 10, minimum: 10}, :allow_blank =>true 
   validates :rmtr_name, presence: true, format: {with: /\A[a-z|A-Z|0-9|\:|\/|\-|\?|\+|\(|\)|\.|\, ]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9|\:|\/|\-|\?|\+|\(|\)|\.|\, ]}'}, length: {maximum: 100, minimum: 2}
   validates :rmtr_address, format: {with: /\A[a-z|A-Z|0-9|\:|\/|\-|\?|\+|\(|\)|\.|\, ]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9|\:|\/|\-|\?|\+|\(|\)|\.|\, ]}'}, length: {maximum: 100, minimum: 2}, :allow_blank =>true
   validates :rmtr_acct_no, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 25, minimum: 1}, :allow_blank =>true
-  validates :rmtr_email, format: {with: /\A[a-z|A-Z|0-9|@|.]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9|@|.]}' }, length: {maximum: 100, minimum: 3}, :allow_blank =>true
   validates :rmtr_mobile, format: {with: /\A[0-9]+\z/, :message => 'Invalid format, expected format is : {[0-9]}'}, length: {maximum: 10, minimum: 10}, :allow_blank =>true
-  validates :invoice_no, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 15, minimum: 1}, :allow_blank =>true
+  validates :invoice_no, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 28, minimum: 1}, :allow_blank =>true
   
   validates :invoice_amt, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => '9e15'.to_f, :allow_nil => true }
   validates :invoice_amt_tol_pct, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100, :allow_nil => true }
@@ -29,7 +28,7 @@ class EcolRemitter < ActiveRecord::Base
   
   validates_uniqueness_of :customer_code, :scope => [:remitter_code, :customer_subcode, :invoice_no]
   
-  validate :validate_customer_subcode_details, :customer_code_should_exist
+  validate :validate_customer_subcode_details, :customer_code_should_exist, :check_email_address
   
   validate :value_types
   validate :mandatory_value
@@ -50,6 +49,21 @@ class EcolRemitter < ActiveRecord::Base
     ecol_customer = EcolCustomer.where(:code => self.customer_code)
     if ecol_customer.nil? 
       errors.add(:customer_code, "Invalid Customer")
+    end
+  end
+  
+  def check_email_address
+    ["customer_subcode_email","rmtr_email"].each do |email_id|
+      invalid_ids = []
+      value = self.send(email_id)
+      unless value.nil?
+        value.split(/;\s*/).each do |email| 
+          unless email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+            invalid_ids << email
+          end
+        end
+      end
+      errors.add(email_id.to_sym, "invalid email #{invalid_ids.join(',')}") unless invalid_ids.empty?
     end
   end
   
