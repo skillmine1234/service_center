cc1b956ec5924b95b71f6ae1fc65417dc826d12e
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-06-27 18:06:45 +0530

    INRW-185 - removing association

--- app/models/ecol_unapproved_record.rb
+++ app/models/ecol_unapproved_record.rb
@@ -1,4 +1,3 @@
 class EcolUnapprovedRecord < ActiveRecord::Base
-  belongs_to :ecol_approvable, :polymorphic => true, :unscoped => true
   ECOL_TABLES = ['EcolCustomer','EcolRemitter','UdfAttribute','EcolRule']
 end
