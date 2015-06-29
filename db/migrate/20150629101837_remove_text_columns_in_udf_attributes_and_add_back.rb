9d64eb91405425995ff54f4f066e0e7d52444a22
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-06-29 15:57:03 +0530

    INRW-258 - migration to move text fields at the end of table

---
+++ db/migrate/20150629101837_remove_text_columns_in_udf_attributes_and_add_back.rb
@@ -0,0 +1,8 @@
+class RemoveTextColumnsInUdfAttributesAndAddBack < ActiveRecord::Migration
+  def change
+    remove_column :udf_attributes, :constraints
+    remove_column :udf_attributes, :select_options
+    add_column :udf_attributes, :constraints, :text
+    add_column :udf_attributes, :select_options, :text
+  end
+end
