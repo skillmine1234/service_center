adbda9da300827076a464e2a9af36f361d8797f9
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-07-04 18:29:44 +0530

    refactoring ecol_remitter

--- app/models/ecol_remitter.rb
+++ app/models/ecol_remitter.rb
@@ -9,30 +9,6 @@ class EcolRemitter < ActiveRecord::Base
   
   belongs_to :ecol_customer
   
-  validates :customer_code, presence: true, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}'}, length: {maximum: 15, minimum: 1}
-  validates :customer_subcode, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}'}, length: {maximum: 15, minimum: 1}, :allow_blank =>true
-  validates :remitter_code, presence: true, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 28, minimum: 1}
-  validates :credit_acct_no, format: {with: /\A[0-9]+\z/, :message => 'Invalid format, expected format is : {[0-9]}' }, length: {maximum: 25, minimum: 10}, :allow_blank =>true
-  validates :customer_subcode_mobile, format: {with: /\A[0-9]+\z/, :message => 'Invalid format, expected format is : {[0-9]}' }, length: {maximum: 10, minimum: 10}, :allow_blank =>true 
-  validates :rmtr_name, presence: true, format: {with: /\A[a-z|A-Z|0-9|\:|\/|\-|\?|\+|\(|\)|\.|\,\s]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9|\:|\/|\-|\?|\+|\(|\)|\.|\,\s]}'}, length: {maximum: 100, minimum: 2}
-  validates :rmtr_address, format: {with: /\A[a-z|A-Z|0-9|\:|\/|\-|\?|\+|\(|\)|\.|\,\s]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9|\:|\/|\-|\?|\+|\(|\)|\.|\,\s]}'}, length: {maximum: 100, minimum: 2}, :allow_blank =>true
-  validates :rmtr_acct_no, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 25, minimum: 1}, :allow_blank =>true
-  validates :rmtr_mobile, format: {with: /\A[0-9]+\z/, :message => 'Invalid format, expected format is : {[0-9]}'}, length: {maximum: 10, minimum: 10}, :allow_blank =>true
-  validates :invoice_no, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}' }, length: {maximum: 28, minimum: 1}, :allow_blank =>true
-  
-  validates :invoice_amt, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => '9e15'.to_f, :allow_nil => true }
-  validates :invoice_amt_tol_pct, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100, :allow_nil => true }
-  validates :min_credit_amt, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => '9e15'.to_f, :allow_nil => true }
-  validates :max_credit_amt, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => '9e15'.to_f, :allow_nil => true }
-  validates :due_date_tol_days, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 28, :allow_nil => true }
-  validates :due_date, presence: true
-  
-  validates_uniqueness_of :customer_code, :scope => [:remitter_code, :customer_subcode, :invoice_no,:approval_status]
-
-  validate :value_types
-  validate :mandatory_value
-  validate :constraints
-  
   def udfs
     UdfAttribute.where("is_enabled=?",'Y').order("id asc")
   end
