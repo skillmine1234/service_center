661af6c8005e7e507568e73d05df00d464810073
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-07-06 14:44:02 +0530

    INRW-281 - migration for patches

---
+++ db/migrate/20150706090135_patch_for_css_changes.rb
@@ -0,0 +1,19 @@
+class PatchForCssChanges < ActiveRecord::Migration
+  def change
+    codes = PurposeCode.all
+    codes.each do |code|
+      code.pattern_beneficiaries.gsub(',','\r\n') unless code.pattern_beneficiaries.nil?
+      code.save(:validate => false)
+    end
+
+    rules = InwRemittanceRule.all
+    rules.each do |rule|
+      rule.pattern_beneficiaries.gsub(',','\r\n') unless rule.pattern_beneficiaries.nil?
+      rule.pattern_corporates.gsub(',','\r\n') unless rule.pattern_corporates.nil?
+      rule.pattern_individuals.gsub(',','\r\n') unless rule.pattern_individuals.nil?
+      rule.pattern_remitters.gsub(',','\r\n') unless rule.pattern_remitters.nil?
+      rule.pattern_salutations.gsub(',','\r\n') unless rule.pattern_salutations.nil?
+      rule.save(:validate => false)
+    end
+  end
+end
