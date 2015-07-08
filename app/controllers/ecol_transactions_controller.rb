8782c2a2a2d4aa79e5945f9c23bb06557bd4d9dc
divyajayan <divya.jayan@quantiguous.com>
2015-07-08 12:29:34 +0530

    INRW-312 Remove create/edit option from Ecol Transactions

--- app/controllers/ecol_transactions_controller.rb
+++ app/controllers/ecol_transactions_controller.rb
@@ -6,37 +6,6 @@ class EcolTransactionsController < ApplicationController
   include ApplicationHelper
   include EcolTransactionsHelper
   
-  def new
-    @ecol_transaction = EcolTransaction.new
-  end
-
-  def create
-    @ecol_transaction = EcolTransaction.new(params[:ecol_transaction])
-    if !@ecol_transaction.valid?
-      render "new"
-    else
-      @ecol_transaction.save
-      flash[:alert] = 'Transaction successfully created'
-      redirect_to @ecol_transaction
-    end
-  end
-
-  def edit
-    @ecol_transaction = EcolTransaction.find_by_id(params[:id])
-  end
-
-  def update
-    @ecol_transaction = EcolTransaction.find_by_id(params[:id])
-    @ecol_transaction.attributes = params[:ecol_transaction]
-    if !@ecol_transaction.valid?
-      render "edit"
-    else
-      @ecol_transaction.save
-      flash[:alert] = 'Transaction successfully modified'
-      redirect_to @ecol_transaction
-    end
-  end
-  
   def index
     ecol_transactions = EcolTransaction.order("id desc")
     if params[:advanced_search].present? || params[:summary].present?
