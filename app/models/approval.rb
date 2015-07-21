fb0baff304c699317d3e2e6c9515bea8452b46f1
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-07-21 14:16:46 +0530

    refactoring approval code

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -3,30 +3,14 @@ module Approval
   included do
     audited except: [:approval_status, :last_action]
 
-    has_one :ecol_unapproved_record, :as => :ecol_approvable
     belongs_to :unapproved_record, :primary_key => 'approved_id', :foreign_key => 'id', :class_name => self.name, :unscoped => true
     belongs_to :approved_record, :foreign_key => 'approved_id', :primary_key => 'id', :class_name => self.name, :unscoped => true
 
     validates_uniqueness_of :approved_id, :allow_blank => true
     validate :validate_unapproved_record
-    
-    after_create :create_ecol_unapproved_records
-    after_update :remove_ecol_unapproved_records
 
     def self.default_scope
       where approval_status: 'A'
-    end
-  end
-
-  def create_ecol_unapproved_records
-    if approval_status == 'U' and ecol_unapproved_record.nil?
-      EcolUnapprovedRecord.create!(:ecol_approvable => self)
-    end
-  end
-
-  def remove_ecol_unapproved_records
-    if approval_status == 'A' and !ecol_unapproved_record.nil?
-      ecol_unapproved_record.delete
     end
   end
 
