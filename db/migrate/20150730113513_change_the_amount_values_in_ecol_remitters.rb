45004604e610a4273216fef17abbecba875574cd
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2015-07-30 17:48:14 +0530

    EC-2 - fixing migration

--- db/migrate/20150730113513_change_the_amount_values_in_ecol_remitters.rb
+++ db/migrate/20150730113513_change_the_amount_values_in_ecol_remitters.rb
@@ -1,9 +1,9 @@
 class ChangeTheAmountValuesInEcolRemitters < ActiveRecord::Migration
   def change
     EcolRemitter.find_each(batch_size: 100) do |remitter|
-      remitter.invoice_amt = remitter.invoice_amt.round(2)
-      remitter.min_credit_amt = remitter.min_credit_amt.round(2)
-      remitter.max_credit_amt = remitter.max_credit_amt.round(2)
+      remitter.invoice_amt = remitter.invoice_amt.round(2) unless remitter.invoice_amt.nil?
+      remitter.min_credit_amt = remitter.min_credit_amt.round(2) unless remitter.min_credit_amt.nil?
+      remitter.max_credit_amt = remitter.max_credit_amt.round(2) unless remitter.max_credit_amt.nil?
       remitter.save(:validate => false)
     end
   end
