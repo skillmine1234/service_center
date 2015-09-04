2545132f162385911783f08c55d0d03e1d44df8b
divyajayan <divya.jayan@quantiguous.com>
2015-09-04 11:23:16 +0530

    migration for ecol_validations

---
+++ db/migrate/20150904054917_create_ecol_validations.rb
@@ -0,0 +1,15 @@
+class CreateEcolValidations < ActiveRecord::Migration
+  def change
+    create_table :ecol_validations do |t|
+      t.integer :ecol_transaction_id
+      t.integer :ecol_customer_id
+      t.string :status_code, :limit => 10
+      t.text :req_bitstream
+      t.text :rep_bitstream
+      t.datetime :req_timestamp, :datetime
+      t.datetime :rep_timestamp, :datetime
+      t.string :fault_code, :string
+      t.string :fault_reason, :string
+    end
+  end
+end
