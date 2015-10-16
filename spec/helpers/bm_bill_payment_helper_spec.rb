bee1f678c7b9fac08f1e34f4e3a9482edd5d9123
divyajayan <divya.jayan@quantiguous.com>
2015-10-16 16:01:38 +0530

    INRW-359 Bill Payment Summary specs

--- spec/helpers/bm_bill_payment_helper_spec.rb
+++ spec/helpers/bm_bill_payment_helper_spec.rb
@@ -56,4 +56,12 @@ describe BmBillPaymentHelper do
       find_logs({:step_name => 'RETURN'},transaction).should == []
     end
   end
+  
+  context "payment_summary_count" do
+    it "should return correct count" do
+      payment_summary_count({["NEW","N"] => 1, ["NEW","Y"] => 1, ["SUCCESS","N"] => 1},['NEW','Y']).should == 1
+      payment_summary_count({["FAILED","N"] => 1, ["SUCCESS","Y"] => 1, ["SUCCESS","N"] => 1},['SUCCESS','Y']).should == 1
+      payment_summary_count({["FAILED","N"] => 1, ["SUCCESS","Y"] => 1, ["SUCCESS","N"] => 1},['FAILED','Y']).should == 0
+    end
+  end
 end
