d7a4474fa24c069d820076ead252870939ec05ea
divyajayan <divya.jayan@quantiguous.com>
2015-10-30 12:06:54 +0530

    fixing error on migration for trace_id_prefix column

--- db/migrate/20151030055840_add_columns_to_bm_rules.rb
+++ db/migrate/20151030055840_add_columns_to_bm_rules.rb
@@ -1,5 +1,5 @@
 class AddColumnsToBmRules < ActiveRecord::Migration
   def change
-    add_column :bm_rules, :traceid_prefix, :integer, :null => false, :default => "Q"
+    add_column :bm_rules, :traceid_prefix, :integer, :null => false, :default => 1
   end
 end
