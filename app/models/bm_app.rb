51cdb30a478ede66ceb39fa797b9ca9df49767c6
divyajayan <divya.jayan@quantiguous.com>
2015-11-19 20:07:25 +0530

    BPAPI-7 specs fix

--- app/models/bm_app.rb
+++ app/models/bm_app.rb
@@ -7,4 +7,6 @@ class BmApp < ActiveRecord::Base
   
   validates_presence_of :app_id, :channel_id
   
+  validates_uniqueness_of :app_id, :scope => :approval_status
+  
 end
--- spec/models/bm_app_spec.rb
+++ spec/models/bm_app_spec.rb
@@ -13,5 +13,10 @@ describe BmApp do
     [:app_id, :channel_id].each do |att|
       it { should validate_presence_of(att) }
     end 
+    
+    it do 
+      bm_app = Factory(:bm_app, :approval_status => 'A')
+      should validate_uniqueness_of(:app_id).scoped_to(:approval_status)   
+    end
   end
 end
