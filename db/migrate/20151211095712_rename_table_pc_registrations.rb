8c9e759cb284f3928263de595fa10d0adb8edc0a
divyajayan <divya.jayan@quantiguous.com>
2015-12-11 15:32:59 +0530

    migration to rename table pc_registrations

---
+++ db/migrate/20151211095712_rename_table_pc_registrations.rb
@@ -0,0 +1,7 @@
+class RenameTablePcRegistrations < ActiveRecord::Migration
+  def change
+    remove_index :pc_registrations, [:req_no, :app_id, :attempt_no]
+    rename_table :pc_registrations, :pc_card_registrations
+    add_index :pc_card_registrations, [:req_no, :app_id, :attempt_no], :unique => true, :name => "card_registrations_unique_constraint"
+  end
+end
