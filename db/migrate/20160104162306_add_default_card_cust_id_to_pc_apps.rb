0b3c8c25c0767a8b8fe44b5b6c4f748cb45617e2
NileshPanchal <neil08.panchal@gmail.com>
2016-01-04 22:23:58 +0530

    default and not null migrations

--- db/migrate/20160104162306_add_default_card_cust_id_to_pc_apps.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultCardCustIdToPcApps < ActiveRecord::Migration
-  def change
-    change_column :pc_apps, :card_cust_id, :string, default: ""
-  end
-end
--- db/migrate/20160104162727_add_default_bene_name_to_pcs_pay_to_accounts.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultBeneNameToPcsPayToAccounts < ActiveRecord::Migration
-  def change
-    change_column :pcs_pay_to_accounts, :bene_name, :string, default: ""
-  end
-end
