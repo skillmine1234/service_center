c496ae801658c16d31cb7d6285162c8bc2b5c04b
NileshPanchal <neil08.panchal@gmail.com>
2016-01-05 10:06:21 +0530

    migrations

--- db/migrate/20160104143825_add_column_card_cust_id_to_pc_apps.rb
+++ db/migrate/20160104143825_add_column_card_cust_id_to_pc_apps.rb
@@ -1,6 +1,12 @@
 class AddColumnCardCustIdToPcApps < ActiveRecord::Migration
   def change
     add_column :pc_apps, :card_cust_id, :string, :limit => 50, :comment => 'the customer that owns the card account no'
-    change_column :pc_apps, :card_cust_id, :string, :null => false , default: ""
+
+    PcApp.all.each do |pc_app|
+      pc_app.card_cust_id = "NA"
+      pc_app.save!
+    end
+
+    change_column :pc_apps, :card_cust_id, :string, :null => false , default: "NA"
   end
 end
--- db/migrate/20160104160054_add_column_bene_name_to_pcs_pay_to_accounts.rb
+++ db/migrate/20160104160054_add_column_bene_name_to_pcs_pay_to_accounts.rb
@@ -1,6 +1,6 @@
 class AddColumnBeneNameToPcsPayToAccounts < ActiveRecord::Migration
   def change
     add_column :pcs_pay_to_accounts, :bene_name, :string, :limit => 255, :comment => 'the name of the beneficiary'
-    change_column :pcs_pay_to_accounts, :bene_name, :string, :null => false, default: ""
+    change_column :pcs_pay_to_accounts, :bene_name, :string, default: "NA"
   end
 end
