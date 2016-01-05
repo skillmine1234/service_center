734ca77d9516fc5d2dae3cdd33b884ea7f48fce6
NileshPanchal <neil08.panchal@gmail.com>
2016-01-05 13:30:23 +0530

    migrations corercted

--- db/migrate/20160104143825_add_column_card_cust_id_to_pc_apps.rb
+++ db/migrate/20160104143825_add_column_card_cust_id_to_pc_apps.rb
@@ -1,12 +1,15 @@
 class AddColumnCardCustIdToPcApps < ActiveRecord::Migration
+
   def change
-    add_column :pc_apps, :card_cust_id, :string, :limit => 50, :comment => 'the customer that owns the card account no'
-
-    PcApp.all.each do |pc_app|
-      pc_app.card_cust_id = "NA"
-      pc_app.save!
-    end
-
-    change_column :pc_apps, :card_cust_id, :string, :null => false , default: "NA"
+    add_column :pc_apps, :card_cust_id, :string
+    db.execute "UPDATE pc_apps SET card_cust_id = 'A' "
+    change_column :pc_apps, :card_cust_id, :string, null: false
   end
+
+  private
+
+  def db
+    ActiveRecord::Base.connection
+  end
+
 end
--- db/migrate/20160104160054_add_column_bene_name_to_pcs_pay_to_accounts.rb
+++ db/migrate/20160104160054_add_column_bene_name_to_pcs_pay_to_accounts.rb
@@ -1,6 +1,13 @@
 class AddColumnBeneNameToPcsPayToAccounts < ActiveRecord::Migration
   def change
     add_column :pcs_pay_to_accounts, :bene_name, :string, :limit => 255, :comment => 'the name of the beneficiary'
-    change_column :pcs_pay_to_accounts, :bene_name, :string, default: "NA"
+    db.execute "UPDATE pcs_pay_to_accounts SET bene_name = 'A' "
+    change_column :pcs_pay_to_accounts, :bene_name, :string, :null => false
+  end
+
+  private
+
+  def db
+    ActiveRecord::Base.connection
   end
 end
