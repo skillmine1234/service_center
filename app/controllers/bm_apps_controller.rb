9cdc1d1d9c80b75226e8df8b52d37ebb6f80a1fb
divyajayan <divya.jayan@quantiguous.com>
2016-01-21 19:28:15 +0530

    BPAPI-107 BmApp Approval

--- app/controllers/bm_apps_controller.rb
+++ app/controllers/bm_apps_controller.rb
@@ -1,5 +1,5 @@
 class BmAppsController < ApplicationController
-  # authorize_resource
+  authorize_resource
   before_filter :authenticate_user!
   before_filter :block_inactive_user!
   respond_to :json
