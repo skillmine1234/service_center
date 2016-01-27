87029a538b58b192a99d58881601b18efc1aecd0
divyajayan <divya.jayan@quantiguous.com>
2016-01-27 17:56:10 +0530

    change in migration for imt_pending_steps

--- db/migrate/20160127115550_change_columns_in_imt_pending_steps.rb
+++ db/migrate/20160127115550_change_columns_in_imt_pending_steps.rb
@@ -3,6 +3,14 @@ class ChangeColumnsInImtPendingSteps < ActiveRecord::Migration
     remove_column :imt_pending_steps, :step_name
     remove_column :imt_pending_steps, :imt_auditable_id
     remove_column :imt_pending_steps, :imt_auditable_type
-    add_column :imt_pending_steps, :imt_audit_step_id, :integer, :default => 1, :null => false
+    add_column :imt_pending_steps, :imt_audit_step_id, :integer, :default => 1
+    db.execute "UPDATE imt_pending_steps SET imt_audit_step_id = 1"
+    change_column :imt_pending_steps, :imt_audit_step_id, :integer, :default => 1, :null => false
+  end
+
+  private
+
+  def db
+    ActiveRecord::Base.connection
   end
 end
