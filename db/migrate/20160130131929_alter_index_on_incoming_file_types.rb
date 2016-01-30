d774db4bf929e6f75a7fa7686f81b9057a2316f4
NileshPanchal <neil08.panchal@gmail.com>
2016-01-30 19:25:44 +0530

    incoming_file_types migration to remove indexes corrected

--- db/migrate/20160130131929_alter_index_on_incoming_file_types.rb
+++ db/migrate/20160130131929_alter_index_on_incoming_file_types.rb
@@ -1,8 +1,8 @@
 class AlterIndexOnIncomingFileTypes < ActiveRecord::Migration
   def change
-    remove_index :incoming_file_types, :name => "I_INC_FIL_TYP_SC_SER_ID"
-    remove_index :incoming_file_types, :name => "I_INCOMING_FILE_TYPES_NAME"
-    remove_index :incoming_file_types, :name => "I_INCOMING_FILE_TYPES_CODE"
+    remove_index :incoming_file_types, :sc_service_id
+    remove_index :incoming_file_types, :name
+    remove_index :incoming_file_types, :code
     add_index :incoming_file_types, [:sc_service_id, :code], :unique => true, :name => 'UK_IN_FILE_TYPES_1'
   end
 end
