8c297fe39de3bc8dbafadb1dc6cbfe3ed248aeec
Neha1993 <neha.upadhyay@quantiguous.com>
2016-01-30 18:54:22 +0530

    modified the uniqueness of code to be for a SC, instead of across SCs

--- app/models/incoming_file_type.rb
+++ app/models/incoming_file_type.rb
@@ -1,4 +1,4 @@
 class IncomingFileType < ActiveRecord::Base
   validates_presence_of :code, :name, :sc_service_id
-  validates_uniqueness_of :code,:name, :sc_service_id
+  validates_uniqueness_of :code, :scope => :sc_service_id
 end
