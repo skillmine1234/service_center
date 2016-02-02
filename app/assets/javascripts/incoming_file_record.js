a9bc198d858aa057e7a14a00e9c6b6f478443d48
NileshPanchal <neil08.panchal@gmail.com>
2016-02-02 14:30:10 +0530

    incoming_file_records - values not getting displayed in modal fixed

--- app/assets/javascripts/incoming_file_record.js
+++ app/assets/javascripts/incoming_file_record.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  $("#show_record_text").on("click", function () {
-    var record_txt = $(this).data('record_txt');
+  $(".show_record_text").on("click", function () {
+    var record_txt = $(this).attr('data-record-txt');
     $("#record_txt").text("");
     $("#record_txt").text(record_txt);
     $('#recordText').modal();
   });
 
-  $("#show_fault_text").on("click", function () {
-    var fault_reason = $(this).data('fault_reason');
-    var fault_code = $(this).data('fault_code');
+  $(".show_fault_text").on("click", function () {
+    var fault_reason = $(this).attr('data-fault-reason');
+    var fault_code = $(this).attr('data-fault-code');
     $(".modal-body .fault_code").text("");
     $(".modal-body .fault_code").text(fault_code);
     $(".modal-body .fault_reason").text("");
@@ -16,8 +16,8 @@ $(document).ready(function(){
     $('#faultText').modal();
   });
 
-  $("#show_fault_bitstream").on("click", function () {
-    var fault_bitstream = $(this).data('fault_bitstream');
+  $(".show_fault_bitstream").on("click", function () {
+    var fault_bitstream = $(this).attr('data-fault-bitstream');
     $("#fault_bitstream").text("");
     $("#fault_bitstream").text(fault_bitstream);
     $('#faultBitstream').modal();
