c579279cc440089aa1c1c7fcaf3f8dd2b0c42336
divyajayan <divya.jayan@quantiguous.com>
2016-03-05 20:34:10 +0530

    schema.rb

--- db/migrate/20160305134959_remove_columns_from_imt_transfers.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsFromImtTransfers < ActiveRecord::Migration
-  def change
-    remove_column :imt_transfers, :initiation_ref_no
-    remove_column :imt_transfers, :cancellation_ref_no
-  end
-end
