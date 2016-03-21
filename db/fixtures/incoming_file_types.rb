8db605e344d8d9ba8dd5af5b9d352fa9f6affa19
Manoj <manoj.kumar@quantiguous.com>
2016-03-21 10:54:35 +0530

    added records

--- db/fixtures/incoming_file_types.rb
+++ db/fixtures/incoming_file_types.rb
@@ -13,6 +13,13 @@ ScService.seed(:code) do |s|
   s.name = 'Instant Money Transfer'
 end
 
+ScService.seed(:code) do |s|
+  s.code = 'SALARY'
+  s.name = 'Salary Processing'
+end
+
+
+
 IncomingFileType.seed(:sc_service_id, :code) do |s|
   s.sc_service_id = ScService.find_by(code: 'AML').id
   s.code = 'SDN'
@@ -106,4 +113,31 @@ IncomingFileType.seed(:sc_service_id, :code) do |s|
   s.skip_first = 'Y'
   s.auto_upload = 'Y'
   s.validate_all = 'N'
+  s.build_response_file = 'N' 
+end
+
+IncomingFileType.seed(:sc_service_id, :code) do |s|
+  s.sc_service_id = ScService.find_by(code: 'SALARY').id
+  s.code = 'SALARY'
+  s.name = 'SALARY'
+  s.msg_domain = 'DFDL'
+  s.msg_model = '{http://www.quantiguous.com/services/file}:paySalary'
+  s.skip_first = 'Y'
+  s.auto_upload = 'Y'
+  s.validate_all = 'Y'
+  s.build_response_file = 'Y' 
+  s.db_unit_name = "pk_qg_su_file_validator.do"
+end
+
+IncomingFileType.seed(:sc_service_id, :code) do |s|
+  s.sc_service_id = ScService.find_by(code: 'PAYSALARY').id
+  s.code = 'PAYSALARY'
+  s.name = 'PAYSALARY'
+  s.msg_domain = 'DFDL'
+  s.msg_model = '{http://www.quantiguous.com/services/file}:paySalary'
+  s.skip_first = 'Y'
+  s.auto_upload = 'Y'
+  s.validate_all = 'Y'
+  s.build_response_file = 'Y' 
+  s.db_unit_name = "pk_qg_su_file_validator.do"
 end
