fa0771cb873fb0d13adb24fcc11f178d07f910af
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-03-29 11:45:39 +0530

    adding default values to su_customers table migration

--- db/migrate/20160328043643_create_su_customers.rb
+++ db/migrate/20160328043643_create_su_customers.rb
@@ -10,9 +10,9 @@ class CreateSuCustomers < ActiveRecord::Migration
       t.string :updated_by, :limit => 20, :comment =>"the person who updates the record"   
       t.datetime :created_at,  :comment =>"the timestamp when the file was created"   
       t.datetime :updated_at,  :comment =>"the timestamp when the record was last updated"   
-      t.integer :lock_version,  :comment =>"the version number of the record every update increments this by 1"   
-      t.string :approval_status, :limit => 1, :comment =>"the indicator to denote whether this record is pending approval or is approved"   
-      t.string :last_action, :limit => 1, :comment =>"the last action create or update that was performed on the record"   
+      t.integer :lock_version,  :default => 0, :comment =>"the version number of the record every update increments this by 1"   
+      t.string :approval_status, :limit => 1, :default => 'U', :comment =>"the indicator to denote whether this record is pending approval or is approved"   
+      t.string :last_action, :limit => 1, :default => 'C', :comment =>"the last action create or update that was performed on the record"   
       t.integer :approved_version,  :comment =>"the version number of the record at the time it was approved"
       t.integer :approved_id,  :comment =>"the id of the record that is being updated"
       t.index([:customer_id,:account_no,:approval_status], :unique => true, :name => "uk_su_customers_1")
