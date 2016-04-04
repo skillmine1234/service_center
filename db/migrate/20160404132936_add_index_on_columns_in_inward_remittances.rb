4e75a1dabd8f18ee81882b7f3d7fa26b6c89b469
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-04-04 20:02:31 +0530

    SUR-51 - changing indexes

--- db/migrate/20160404132936_add_index_on_columns_in_inward_remittances.rb
+++ db/migrate/20160404132936_add_index_on_columns_in_inward_remittances.rb
@@ -1,9 +1,17 @@
 class AddIndexOnColumnsInInwardRemittances < ActiveRecord::Migration
   def up
-    add_index(:inward_remittances, [:status_code, :bank_ref, :bene_account_no, :req_transfer_type, :transfer_type], name: "idx_inrw_columns")
+    add_index :inward_remittances, :status_code
+    add_index :inward_remittances, :bank_ref
+    add_index :inward_remittances, :bene_account_no
+    add_index :inward_remittances, :req_transfer_type
+    add_index :inward_remittances, :transfer_type
   end
 
   def down
-    remove_index(:inw_remittances, :name => 'idx_inrw_columns')
+    remove_index :inward_remittances, :status_code
+    remove_index :inward_remittances, :bank_ref
+    remove_index :inward_remittances, :bene_account_no
+    remove_index :inward_remittances, :req_transfer_type
+    remove_index :inward_remittances, :transfer_type
   end
 end
--- db/migrate/20160404133731_add_index_on_columns_in_funds_transfers.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexOnColumnsInFundsTransfers < ActiveRecord::Migration
-  def up
-    add_index(:funds_transfers, [:status_code, :bank_ref, :bene_account_no, :req_transfer_type, :transfer_type], name: "idx_ft_columns")
-  end
-
-  def down
-    remove_index(:funds_transfers, :name => 'idx_ft_columns')
-  end
-end
