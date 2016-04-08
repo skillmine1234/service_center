d954fd0a8b23682f2715a7eb115b2b4ce873c9db
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-04-08 14:18:46 +0530

    changing the migration for rep_fault_code

--- db/migrate/20160408061746_change_rep_fault_code_in_incoming_file_records.rb
+++ db/migrate/20160408061746_change_rep_fault_code_in_incoming_file_records.rb
@@ -1,5 +1,11 @@
 class ChangeRepFaultCodeInIncomingFileRecords < ActiveRecord::Migration
   def change
-    change_column :incoming_file_records, :rep_fault_code, :string, :limit => 50, :comment => "the code of the fault that was generated while creating/writing the response record"
+    add_column :incoming_file_records, :rep_fault_code1, :string, :limit => 50, :comment => "the code of the fault that was generated while creating/writing the response record"
+    IncomingFileRecord.find_each(batch_size: 100) do |incoming_file_record|
+      incoming_file_record.rep_fault_code1 = incoming_file_record.rep_fault_code
+      incoming_file_record.save
+    end
+    remove_column :incoming_file_records, :rep_fault_code
+    rename_column :incoming_file_records, :rep_fault_code1, :rep_fault_code
   end
 end
