5ff2d62367b4323d0b714454da75456c632f74b9
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-04-11 18:18:45 +0530

    fixing migration

--- db/migrate/20160405050549_create_ic_incoming_files.rb
+++ db/migrate/20160405050549_create_ic_incoming_files.rb
@@ -5,6 +5,6 @@ class CreateIcIncomingFiles < ActiveRecord::Migration
       t.string :corp_customer_id, :limit => 15, :comment => "the customer id of the corporate, who is paying the supplier od"
       t.string :pm_utr, :limit => 64, :comment => "the transaction unique ref No of NEFT done by corporate"
     end
-    add_index :ic_incoming_files, :incoming_file_id, :name => "ic_file_index", :unique => true
+    add_index :ic_incoming_files, :file_name, :name => "ic_file_index", :unique => true
   end
 end
