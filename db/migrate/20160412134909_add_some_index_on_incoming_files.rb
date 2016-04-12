1f3cad3fecf377ff89ba222f4660932e1b59d025
Neha1993 <neha.upadhyay@quantiguous.com>
2016-04-12 19:57:04 +0530

    improved on the index name : follow conventions

--- db/migrate/20160412134909_add_some_index_on_incoming_files.rb
+++ db/migrate/20160412134909_add_some_index_on_incoming_files.rb
@@ -1,5 +1,5 @@
 class AddSomeIndexOnIncomingFiles < ActiveRecord::Migration
   def change
-    add_index :incoming_files, [:service_name, :status, :pending_approval], :name => "index_on_service"
+    add_index :incoming_files, [:service_name, :status, :pending_approval], :name => "in_incoming_files_2"
   end
 end
