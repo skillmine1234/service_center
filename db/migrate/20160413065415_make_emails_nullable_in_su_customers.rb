86337dab6744b2b6f4f4dce5bd0c58b52a7836ba
NileshPanchal <neil08.panchal@gmail.com>
2016-04-13 14:52:07 +0530

    ic, su - comments added for emails and mob_no fields

--- db/migrate/20160413065415_make_emails_nullable_in_su_customers.rb
+++ db/migrate/20160413065415_make_emails_nullable_in_su_customers.rb
@@ -1,6 +1,6 @@
 class MakeEmailsNullableInSuCustomers < ActiveRecord::Migration
   def change
-    change_column :su_customers, :ops_email, :string, :null => true
-    change_column :su_customers, :rm_email, :string, :null => true
+    change_column :su_customers, :ops_email, :string, :null => true, :comment => "the email address of the operations team, emails are sent when customer files are rejected"
+    change_column :su_customers, :rm_email, :string, :null => true, :comment => "the email address of the relationship manager, emails are sent when fresh credits are rejected"
   end
 end
--- db/migrate/20160413070502_make_emails_and_mob_no_nullable_in_ic_customers.rb
+++ db/migrate/20160413070502_make_emails_and_mob_no_nullable_in_ic_customers.rb
@@ -1,8 +1,8 @@
 class MakeEmailsAndMobNoNullableInIcCustomers < ActiveRecord::Migration
   def change
-    change_column :ic_customers, :cust_contact_mobile, :string, :null => true
-    change_column :ic_customers, :cust_contact_email, :string, :null => true
-    change_column :ic_customers, :ops_email, :string, :null => true
-    change_column :ic_customers, :rm_email, :string, :null => true
+    change_column :ic_customers, :cust_contact_mobile, :string, :null => true, :comment => "the mobile no of the customer''s contact"
+    change_column :ic_customers, :cust_contact_email, :string, :null => true, :comment => "the email address of the customer''s contact"
+    change_column :ic_customers, :ops_email, :string, :null => true, :comment => "the email address of the operations team, emails are sent when customer files are rejected"
+    change_column :ic_customers, :rm_email, :string, :null => true, :comment => "the email address of the relationship manager, emails are sent when fresh credits are rejected"
   end
 end
