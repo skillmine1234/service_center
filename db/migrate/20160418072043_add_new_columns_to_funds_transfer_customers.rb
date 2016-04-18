ea608c0c8e92858cd52d2df837e22301f408d786
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-04-18 19:35:28 +0530

    fixing migrations

--- db/migrate/20160418072043_add_new_columns_to_funds_transfer_customers.rb
+++ db/migrate/20160418072043_add_new_columns_to_funds_transfer_customers.rb
@@ -2,12 +2,15 @@ class AddNewColumnsToFundsTransferCustomers < ActiveRecord::Migration
   def change
     add_column :funds_transfer_customers, :is_retail, :string, :limit => 1, :comment => "the flag to identify whether app_id is for retail or corporate customer"
     add_column :funds_transfer_customers, :allow_rtgs, :string, :limit => 1, :comment => "the flag to identify whether rtgs is allowed for the app_id or not"
-    add_column :funds_transfer_customers, :app_id, :string, :limit => 20, :comment => "the unique id assigned to a client app"
     if Rails.configuration.database_configuration[Rails.env]["adapter"] == 'sqlite3'
+      add_column :funds_transfer_customers, :app_id, :string, :limit => 20, :comment => "the unique id assigned to a client app"
       db.execute "UPDATE funds_transfer_customers SET app_id = 'a'" 
+      add_index :funds_transfer_customers, [:app_id,:approval_status], :unique => true, :name => "FT_cust_index_on_app_id"
+      change_column :funds_transfer_customers, :app_id, :string, :limit => 20, :null => false, :comment => "the unique id assigned to a client app"    
+    else
+      change_column :funds_transfer_customers, :app_id, :string, :limit => 20, :null => false, :comment => "the unique id assigned to a client app"    
+      add_index :funds_transfer_customers, [:app_id,:approval_status], :unique => true, :name => "FT_cust_index_on_app_id"
     end
-    add_index :funds_transfer_customers, [:app_id,:approval_status], :unique => true, :name => "FT_cust_index_on_app_id"
-    change_column :funds_transfer_customers, :app_id, :string, :limit => 20, :null => false, :comment => "the unique id assigned to a client app"    
     change_column :funds_transfer_customers, :customer_id, :string, :null => true, :comment => "the ID of the customer"
   end
 
