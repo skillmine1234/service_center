bb9e573ab8035d54a4eff9dd308868271e8a512b
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-04-20 14:54:00 +0530

    fixing migrations

--- db/migrate/20160420061021_change_index_on_ft_customers.rb
+++ db/migrate/20160420061021_change_index_on_ft_customers.rb
@@ -1,7 +1,7 @@
 class ChangeIndexOnFtCustomers < ActiveRecord::Migration
   def change
-    remove_index :ft_customers, :column => [:customer_id,:approval_status],:name => "FT_cust_index_on_app_id"
-    remove_index :ft_customers, :column => [:app_id,:approval_status], :name => "FT_cust_index_on_customer_id"
+    remove_index :ft_customers, :name => "FT_cust_index_on_app_id"
+    remove_index :ft_customers, :name => "FT_cust_index_on_customer_id"
     add_index :ft_customers, [:app_id,:customer_id,:approval_status], :unique => true, :name => "FT_cust_index_on_app_id"
   end
 end
