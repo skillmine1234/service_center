1a42ff3a2f8327dd1d958c58bb2ed21dbbbb0e7d
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-04-20 17:41:37 +0530

    fixing issue in migration

--- db/migrate/20160420051921_rename_funds_transfer_customers.rb
+++ db/migrate/20160420051921_rename_funds_transfer_customers.rb
@@ -1,8 +1,5 @@
 class RenameFundsTransferCustomers < ActiveRecord::Migration
   def up
-    remove_index :funds_transfer_customers, :name
-    remove_index :funds_transfer_customers, :name => "FT_cust_index_on_app_id"
-    remove_index :funds_transfer_customers, :name => "FT_cust_index_on_customer_id"
     rename_table :funds_transfer_customers, :ft_customers
     add_index :ft_customers, :name
     add_index :ft_customers, [:app_id, :customer_id, :approval_status], :name => "FT_cust_index_on_app_id"
