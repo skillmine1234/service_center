ba49571f36ba88f8d22f11b36a9c61dd99310417
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-06-20 14:54:52 +0530

    migrations for smb tables

---
+++ db/migrate/20160620063111_create_sm_banks.rb
@@ -0,0 +1,24 @@
+class CreateSmBanks < ActiveRecord::Migration
+  def change
+    create_table :sm_banks, {:sequence_start_value => '1 cache 20 order increment by 1'} do |t|
+      t.string :code, :null => false, :limit => 20, :comment => "the unique code assigned to the sub member" 
+      t.string :name, :null => false, :limit => 100, :comment => "the name of the sub member"
+      t.string :bank_code, :null => false, :limit => 20, :comment => "the bank code of the sub member, all incoming requests are validated against this"
+      t.number :low_balance_alert_at, :null => false, :comment => "the minimum balance that the smb should maintain to avoid alerts"
+      t.string :identity_user_id, :null => false, :limit => 20, :comment => "the minimum balance that the smb should maintain to avoid alerts"
+      t.string :neft_allowed, :null => false, :limit => 1, :comment => "the flag to indicate if NEFT is allowed"
+      t.string :imps_allowed, :null => false, :limit => 1, :comment => "the flag to indicate if IMPS is allowed"
+      t.string :created_by, :limit => 20, :comment => "the person who creates the record"
+      t.string :updated_by, :limit => 20, :comment => "the person who updates the record"
+      t.datetime :created_at, :null => false, :comment => "the timestamp when the record was created"
+      t.datetime :updated_at, :null => false, :comment => "the timestamp when the record was last updated"
+      t.integer :lock_version, :null => false, :default => 0, :comment => "the version number of the record, every update increments this by 1"
+      t.string :approval_status, :limit => 1, :default => 'U', :null => false, :comment => "the indicator to denote whether this record is pending approval or is approved"
+      t.string :last_action, :limit => 1, :default => 'C', :null => false, :comment => "the last action (create, update) that was performed on the record"
+      t.integer :approved_version, :comment => "the version number of the record, at the time it was approved"
+      t.integer :approved_id, :comment => "the id of the record that is being updated"
+    end
+    add_index :sm_banks, [:code, :approval_status], :unique => true, :name => "sm_banks_01"
+    add_index :sm_banks, [:name,:bank_code], :name => "sm_banks_02"
+  end
+end
---
+++ db/migrate/20160620090907_create_sm_audit_steps.rb
@@ -0,0 +1,23 @@
+class CreateSmAuditSteps < ActiveRecord::Migration
+  def change
+    create_table :sm_audit_steps, {:sequence_start_value => '1 cache 20 order increment by 1'} do |t|
+      t.string :sm_auditable_type, :null => false, :comment => "the name of the table that represents the request that is related to this record"
+      t.integer :sm_auditable_id, :null => false, :comment => "the id of the row that represents the request that is related to this record"
+      t.integer :step_no, :null => false, :comment => "the step of the transaction, for which this record exists"
+      t.integer :attempt_no, :null => false , :comment => "the attempt number of the request, failed requests can be retried"
+      t.string :step_name, :limit => 100, :null => false, :comment => "the english name of the step: create user, request token, access token, modify user, add address, add card, activate card"
+      t.string :status_code, :limit => 25, :null => false, :comment => "the status of this attempt of the step"
+      t.string :fault_code, :limit => 255, :comment => "the code that identifies the exception, if an exception occured in the ESB"
+      t.string :fault_subcode, :limit => 50, :comment => "the error code that the third party will return"
+      t.string :fault_reason, :limit => 1000, :comment => "the english reason of the exception, if an exception occurred in the ESB"
+      t.string :req_reference, :limit => 255, :comment => "the reference number that was sent to the service provider"
+      t.datetime :req_timestamp, :comment => "the SYSDATE when the request was sent to the service provider"
+      t.string :rep_reference, :comment => "the reference number as received from ther service provider"
+      t.datetime :rep_timestamp, :comment => "the SYSDATE when the reply was sent to the client"
+      t.text :req_bitstream, :comment => "the full request payload as received from the client"
+      t.text :rep_bitstream, :comment => "the full reply payload as sent to the client"
+      t.text :fault_bitstream, :comment => "the complete exception list/stack trace of an exception that occured in the ESB"  
+      t.index([:sm_auditable_type, :sm_auditable_id, :step_no, :attempt_no], :unique => true, :name => "uk_sm_audit_steps")
+    end
+  end
+end
