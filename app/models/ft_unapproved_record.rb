06e8cce1a0c202f4cdcb0cf90092d9a4a7c847fe
Lavanya <lavanya.ramamoorthy@quantiguous.com>
2016-06-20 16:15:52 +0530

    adding incoming file in ft approval screen

--- app/models/ft_unapproved_record.rb
+++ app/models/ft_unapproved_record.rb
@@ -1,4 +1,4 @@
 class FtUnapprovedRecord < ActiveRecord::Base
   belongs_to :ft_approvable, :polymorphic => true, :unscoped => true
-  FT_TABLES = ['FundsTransferCustomer', 'FtPurposeCode']
+  FT_TABLES = ['FundsTransferCustomer', 'FtPurposeCode','IncomingFile']
 end
--- spec/helpers/ft_unapproved_records_helper_spec.rb
+++ spec/helpers/ft_unapproved_records_helper_spec.rb
@@ -4,7 +4,7 @@ describe FtUnapprovedRecordsHelper do
   context "filter_records" do 
     it "should return records" do
       ft_unapproved_record = Factory(:ft_unapproved_record, :ft_approvable_type => 'FundsTransferCustomer')
-      filter_records(FtUnapprovedRecord).should eq([{:record_type=>"FundsTransferCustomer", :record_count=>1}, {:record_type=>"FtPurposeCode", :record_count=>0}])
+      filter_records(FtUnapprovedRecord).should eq([{:record_type=>"FundsTransferCustomer", :record_count=>1}, {:record_type=>"FtPurposeCode", :record_count=>0}, {:record_type=>"IncomingFile", :record_count=>0}])
     end
   end
 end
