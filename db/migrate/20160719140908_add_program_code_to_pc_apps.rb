fa05fd05cebdc2bede741b7191cc3b36433ae2eb
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2016-08-05 19:25:49 +0530

    pc migration fixes

---
+++ db/migrate/20160719140908_add_program_code_to_pc_apps.rb
@@ -0,0 +1,13 @@
+class AddProgramCodeToPcApps < ActiveRecord::Migration
+  def change
+    add_column :pc_apps, :program_code, :string, :limit => 15, :comment => "the code that identifies the program"
+    db.execute "UPDATE pc_apps SET program_code = A"
+    change_column :pc_apps, :program_code, :string, :limit => 15, :null => false, :comment => "the code that identifies the program"
+  end
+
+  private
+
+  def db
+    ActiveRecord::Base.connection
+  end
+end
