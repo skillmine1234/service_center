1f664c7d07a12f50c9d03d13912f57859527e40b
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2016-08-05 19:30:25 +0530

    fixing codeship

--- db/migrate/20160727111643_add_program_code_to_pc_fee_rules.rb
+++ db/migrate/20160727111643_add_program_code_to_pc_fee_rules.rb
@@ -1,7 +1,7 @@
 class AddProgramCodeToPcFeeRules < ActiveRecord::Migration
   def change
     add_column :pc_fee_rules, :program_code, :string, :limit => 15, :comment => "the code that identifies the program"
-    db.execute "UPDATE pc_apps SET program_code = A"
+    db.execute "UPDATE pc_apps SET program_code = 'A'"
     change_column :pc_fee_rules, :program_code, :string, :limit => 15, :null => false, :comment => "the code that identifies the program"
   end
 
