0fe0fc1e09927dea17049a6f0afa17655f63afdb
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2016-08-09 19:26:40 +0530

    SY-27 - pc advanced search changes

--- spec/helpers/pc_apps_helper_spec.rb
+++ spec/helpers/pc_apps_helper_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe PcAppsHelper do
   context "find_pc_apps" do
     it "should find pc_apps" do
-      pc_program = Factory(:pc_program, :approval_status => 'A')
+      pc_program = Factory(:pc_program, :approval_status => 'A', :code => 'pcapp1')
       pc_app = Factory(:pc_app, :program_code=> pc_program.code, :approval_status => 'A')
-      find_pc_apps({:program_code => pc_app.program_code}).should == [pc_app]
-      find_pc_apps({:program_code => '1111'}).should == []
+      find_pc_apps({:program_code => 'pcapp1'}).should == [pc_app]
+      find_pc_apps({:program_code => 'PCapp1'}).should == [pc_app]
+      find_pc_apps({:program_code => 'pcapp2'}).should == []
 
       pc_app = Factory(:pc_app, :app_id => '1234', :approval_status => 'A')
       find_pc_apps({:app_id => '1234'}).should == [pc_app]
--- spec/helpers/pc_fee_rules_helper_spec.rb
+++ spec/helpers/pc_fee_rules_helper_spec.rb
@@ -4,8 +4,9 @@ describe PcFeeRulesHelper do
   context "find_pc_fee_rules" do
     it "should find pc_fee_rules" do
       pc_program = Factory(:pc_program, :approval_status => 'A')
-      pc_fee_rule = Factory(:pc_fee_rule, :program_code => pc_program.code, :approval_status => 'A')
-      find_pc_fee_rules({:program_code => pc_fee_rule.program_code}).should == [pc_fee_rule]
+      pc_fee_rule = Factory(:pc_fee_rule, :program_code => 'pc123', :approval_status => 'A')
+      find_pc_fee_rules({:program_code => 'pc123'}).should == [pc_fee_rule]
+      find_pc_fee_rules({:program_code => 'PC123'}).should == [pc_fee_rule]
       find_pc_fee_rules({:program_code => '1111'}).should == []
     end
   end
