bd6d22e57c71317164f50fb6c7c857afd9fc8783
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2016-09-28 13:10:11 +0530

    migration changes for cnb

--- db/migrate/20160923154255_create_cn_incoming_files.rb
+++ db/migrate/20160923154255_create_cn_incoming_files.rb
@@ -1,12 +1,13 @@
 class CreateCnIncomingFiles < ActiveRecord::Migration
   def change
-    create_table :cn_incoming_files do |t|
+    create_table :cn_incoming_files, {:sequence_start_value => '1 cache 20 order increment by 1'} do |t|
       t.string  :file_name, :limit => 50, :comment => "the name of the incoming_file"    
       t.string  :batch_no, :limit => 20,  :comment => "the batch number of the incoming_file"  
-      t.string  :rej_file_name, :limit => 50,  :comment => "the name of the cnb response file"  
-      t.string  :rej_file_path, :limit => 255,  :comment => "the path of the cnb response file"  
+      t.string  :rej_file_name, :limit => 50,  :comment => "the name of the rejected response file"  
+      t.string  :rej_file_path, :limit => 255,  :comment => "the path of the rejected response file"  
       t.string  :cnb_file_name, :limit => 50,  :comment => "the name of the cnb response file"  
       t.string  :cnb_file_path, :limit => 255,  :comment => "the path of the cnb response file"  
+      t.index([:file_name], :unique => true, :name => 'cn_incoming_files_01')
     end
   end
 end
