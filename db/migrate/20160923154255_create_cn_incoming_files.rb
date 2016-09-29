02beef7b47503a66a4bda52f58462cd808721783
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2016-09-29 16:57:16 +0530

    adding missed rej_file_path field in cn_incoming_files table

--- db/migrate/20160923154255_create_cn_incoming_files.rb
+++ db/migrate/20160923154255_create_cn_incoming_files.rb
@@ -4,7 +4,7 @@ class CreateCnIncomingFiles < ActiveRecord::Migration
       t.string  :file_name, :limit => 50, :comment => "the name of the incoming_file"    
       t.string  :batch_no, :limit => 20,  :comment => "the batch number of the incoming_file"  
       t.string  :rej_file_name, :limit => 50,  :comment => "the name of the rejected response file"  
-      t.string  :rej_file_name, :limit => 50,  :comment => "the name of the rejected response file"       
+      t.string  :rej_file_path, :limit => 255,  :comment => "the path of the rejected response file"       
       t.string  :rej_file_status, :limit => 255,  :comment => "the status of the rejected response file"  
       t.string  :cnb_file_name, :limit => 50,  :comment => "the name of the cnb response file"  
       t.string  :cnb_file_path, :limit => 255,  :comment => "the path of the cnb response file"
