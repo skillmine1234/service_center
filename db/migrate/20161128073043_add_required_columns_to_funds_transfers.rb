d04b75e17595922deeeb3b536d562e0b3914daea
Neha1993 <neha.upadhyay@quantiguous.com>
2016-12-02 06:44:45 +0530

    added new tables for funds transfer and new columns in the existing funds transfer tables

---
+++ db/migrate/20161128073043_add_required_columns_to_funds_transfers.rb
@@ -0,0 +1,9 @@
+class AddRequiredColumnsToFundsTransfers < ActiveRecord::Migration
+  def change
+    add_column :funds_transfers, :notify_status, :string, :limit => 100, :comment => "the status of the notify step for e.g., NOTIFIED:OK, NOTIFY:REJECTED, NOTIFICATION FAILED and PENDING NOTIFICATION"        
+    add_column :funds_transfers, :notify_attempt_no, :integer, :comment => 'the attempt no of the notify step'  
+    add_column :funds_transfers, :notify_attempt_at, :datetime, :comment => 'the last attempt of the notify step'  
+    add_column :funds_transfers, :notified_at, :datetime, :comment => 'the timestamp when the notification was happened'  
+    add_column :funds_transfers, :notify_result, :string, :limit => 50, :comment => 'the failure reason of the notify step'     	
+  end
+end
