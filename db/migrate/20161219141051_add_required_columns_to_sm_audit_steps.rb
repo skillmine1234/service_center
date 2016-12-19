3f330bee6a3129f70da6641d6cf77cebddb982b2
Neha1993 <neha.upadhyay@quantiguous.com>
2016-12-19 19:48:31 +0530

    added required table for domestic remittances

---
+++ db/migrate/20161219141051_add_required_columns_to_sm_audit_steps.rb
@@ -0,0 +1,8 @@
+class AddRequiredColumnsToSmAuditSteps < ActiveRecord::Migration
+  def change
+	add_column :sm_audit_steps, :remote_host, :string, :limit => 500, :comment => "the URL of the calling service"        
+	add_column :sm_audit_steps, :req_uri, :string, :limit => 500, :comment => "the URI of the calling service"   
+	add_column :sm_audit_steps, :req_header, :text, :comment => "the header which will be passed along with the request"        
+	add_column :sm_audit_steps, :rep_header, :text, :comment => "the header which comes after calling the service"            
+  end
+end
