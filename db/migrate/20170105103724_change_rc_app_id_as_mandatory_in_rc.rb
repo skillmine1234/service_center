dd4d0d930dffb6546a8e3c977343b1b2430c80d4
divyajayan <divya.jayan@quantiguous.com>
2017-01-07 15:44:03 +0530

    RTV-19 default value for old rc_app_id

--- db/migrate/20170105103724_change_rc_app_id_as_mandatory_in_rc.rb
+++ db/migrate/20170105103724_change_rc_app_id_as_mandatory_in_rc.rb
@@ -1,6 +1,6 @@
 class ChangeRcAppIdAsMandatoryInRc < ActiveRecord::Migration
   def change
-    change_column :rc_transfer_schedule, :rc_app_id, :integer, null: false
-    change_column :rc_transfers, :rc_app_id, :integer, null: false
+    change_column :rc_transfer_schedule, :rc_app_id, :integer, null: false, default: 10000
+    change_column :rc_transfers, :rc_app_id, :integer, null: false, default: 10000
   end
 end
