bed8ba2d226644c16fa36dd326e10c276ca28f7e
divyajayan <divya.jayan@quantiguous.com>
2017-01-07 16:13:29 +0530

    RTV-19 removed default value

--- db/migrate/20170105103724_change_rc_app_id_as_mandatory_in_rc.rb
+++ db/migrate/20170105103724_change_rc_app_id_as_mandatory_in_rc.rb
@@ -1,6 +1,8 @@
 class ChangeRcAppIdAsMandatoryInRc < ActiveRecord::Migration
   def change
-    change_column :rc_transfer_schedule, :rc_app_id, :integer, null: false, default: 10000
-    change_column :rc_transfers, :rc_app_id, :integer, null: false, default: 10000
+    RcTransferSchedule.unscoped.update_all(rc_app_id: 10000)
+    RcTransfer.update_all(rc_app_id: 10000)
+    change_column :rc_transfer_schedule, :rc_app_id, :integer, null: false
+    change_column :rc_transfers, :rc_app_id, :integer, null: false
   end
 end
