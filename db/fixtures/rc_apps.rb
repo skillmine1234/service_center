bb26abef7770eab2f200651932d5b8b4d5847ae9
Neha1993 <neha.upadhyay@quantiguous.com>
2017-02-21 10:14:59 +0530

    seed for tata power (#61)

--- db/fixtures/rc_apps.rb
+++ db/fixtures/rc_apps.rb
@@ -25,3 +25,19 @@ RcApp.seed(:app_id) do |s|
   s.created_by = 'Q'
   s.approval_status = 'A'
 end
+
+RcApp.seed(:app_id) do |s|
+  s.id = 10002
+  s.app_id = 'TATAPOWER'
+  s.url = 'https://www.tatapower-ddl.com/h2hlinkagetest/Service.asmx'
+  s.udf1_name  = 'account_desc'
+  s.udf1_type  = 'text'
+  s.setting1_name = 'secure_string'
+  s.setting1_type = 'text'
+  s.setting1_value = 'H2HYESBANK'
+  s.setting2_name = 'password'
+  s.setting2_type = 'text'
+  s.setting2_value = 'yesbank@12345'
+  s.created_by = 'Q'
+  s.approval_status = 'A'
+end
