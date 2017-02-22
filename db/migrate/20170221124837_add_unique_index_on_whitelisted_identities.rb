a0f76b28d54a1219bd23a5bbba1d19b8652de47c
divyajayan <divya.jayan@quantiguous.com>
2017-02-22 18:07:10 +0530

    unwanted migration file removed

--- db/migrate/20170221124837_add_unique_index_on_whitelisted_identities.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueIndexOnWhitelistedIdentities < ActiveRecord::Migration
-  def change
-    add_index :whitelisted_identities, :rmtr_code, unique: true, name: 'uk_whitelisted_01'
-  end
-end
