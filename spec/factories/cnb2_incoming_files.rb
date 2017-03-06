4b7d341c2abe83bb6cb236f174d072e596d72d2a
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2017-03-06 10:49:42 +0000

    cnb2 - removing account from cnb2 incoming files UI

--- spec/factories/cnb2_incoming_files.rb
+++ spec/factories/cnb2_incoming_files.rb
@@ -3,7 +3,6 @@
 FactoryGirl.define do
   factory :cnb2_incoming_file do
     file_name {Factory(:incoming_file).file_name}
-    account 1
     cnb_file_name 'File2'
     cnb_file_path '/public'
     cnb_file_status 'C'
