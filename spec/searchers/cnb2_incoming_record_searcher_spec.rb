740ff9e2adbd689ca82e0269420be374651da2b7
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2017-03-10 14:24:35 +0000

    CNB-25 - advanced search changes

--- spec/searchers/cnb2_incoming_record_searcher_spec.rb
+++ spec/searchers/cnb2_incoming_record_searcher_spec.rb
@@ -14,9 +14,9 @@ describe Cnb2IncomingRecordSearcher do
       Cnb2IncomingRecordSearcher.new({vendor_code: '2222',file_name: cnb2_incoming_record.file_name, status: 'FAILED'}).paginate.should == [cnb2_incoming_record]
       Cnb2IncomingRecordSearcher.new({vendor_code: '2223',file_name: cnb2_incoming_record.file_name, status: 'FAILED'}).paginate.should == []
 
-      cnb2_incoming_record = Factory(:cnb2_incoming_record, :bene_account_no => '1234567')
+      cnb2_incoming_record = Factory(:cnb2_incoming_record, :bene_account_no => '1234567  ')
       Cnb2IncomingRecordSearcher.new({bene_account_no: '1234567',file_name: cnb2_incoming_record.file_name, status: 'FAILED'}).paginate.should == [cnb2_incoming_record]
-      Cnb2IncomingRecordSearcher.new({bene_account_no: '12345',file_name: cnb2_incoming_record.file_name, status: 'FAILED'}).paginate.should == []
+      Cnb2IncomingRecordSearcher.new({bene_account_no: '5432',file_name: cnb2_incoming_record.file_name, status: 'FAILED'}).paginate.should == []
     end
   end
 end
