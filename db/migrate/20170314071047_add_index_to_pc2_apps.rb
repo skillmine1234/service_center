47ded23de03cd9b21b68624b9888dfeff3adfeeb
naveen <naveen.pokuri@quantiguous>
2017-03-14 20:22:58 +0530

    PC2-47 : made changes as per review comments

--- db/migrate/20170314071047_add_index_to_pc2_apps.rb
+++ db/migrate/20170314071047_add_index_to_pc2_apps.rb
@@ -1,5 +1,6 @@
 class AddIndexToPc2Apps < ActiveRecord::Migration
   def change
-    add_index :pc2_apps, [:app_id, :approval_status, :customer_id], :unique => true, :name => "pc2_apps_01"
+     remove_index :pc2_apps, [:app_id, :approval_status]
+     add_index :pc2_apps, [:app_id, :customer_id, :approval_status], :unique => true, :name => "pc2_apps_01"
   end
 end
