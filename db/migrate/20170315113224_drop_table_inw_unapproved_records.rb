016e11ab3710bb849dc2e70ebcd62d945dab967a
NileshPanchal <neil08.panchal@gmail.com>
2017-03-15 19:46:48 +0530

    migration for copying data from inw_unapproved_records to unapproved_records corrected

--- db/migrate/20170315113224_drop_table_inw_unapproved_records.rb
+++ db/migrate/20170315113224_drop_table_inw_unapproved_records.rb
@@ -1,6 +1,7 @@
 class DropTableInwUnapprovedRecords < ActiveRecord::Migration
   def change
-    db.execute "insert into unapproved_records select * from inw_unapproved_records"
+    db.execute "INSERT INTO unapproved_records (id, approvable_id, approvable_type, created_at, updated_at) 
+    SELECT id, inw_approvable_id, inw_approvable_type, created_at, updated_at FROM inw_unapproved_records"
     drop_table :inw_unapproved_records
   end
   
