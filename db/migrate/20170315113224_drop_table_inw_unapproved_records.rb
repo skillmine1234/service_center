b59f35e30f83363f6b3cafa555612438af315a26
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2017-03-15 20:21:42 +0000

    migration fix inw approval

--- db/migrate/20170315113224_drop_table_inw_unapproved_records.rb
+++ db/migrate/20170315113224_drop_table_inw_unapproved_records.rb
@@ -1,10 +1,18 @@
 class DropTableInwUnapprovedRecords < ActiveRecord::Migration
-  def change
-    db.execute "INSERT INTO unapproved_records (approvable_id, approvable_type, created_at, updated_at) 
-    SELECT inw_approvable_id, inw_approvable_type, created_at, updated_at FROM inw_unapproved_records"
+  def up
+    if Rails.configuration.database_configuration[Rails.env]["adapter"] == 'oracle_enhanced'
+      db.execute "INSERT INTO unapproved_records (id, approvable_id, approvable_type, created_at, updated_at) 
+      SELECT unapproved_records_seq.nextval, inw_approvable_id, inw_approvable_type, created_at, updated_at FROM inw_unapproved_records"
+    else
+      db.execute "INSERT INTO unapproved_records (approvable_id, approvable_type, created_at, updated_at) 
+      SELECT inw_approvable_id, inw_approvable_type, created_at, updated_at FROM inw_unapproved_records"
+    end
     drop_table :inw_unapproved_records
   end
   
+  def down
+
+  end
   private
 
   def db
