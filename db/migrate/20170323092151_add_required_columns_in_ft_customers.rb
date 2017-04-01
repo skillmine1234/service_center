add09bc680c64e8bb836af4eb13c5f7e96b26010
talarivenugopal <venugopal.talari@quantiguous.com>
2017-04-01 18:28:50 +0530

    corrected column names

--- db/migrate/20170323092151_add_required_columns_in_ft_customers.rb
+++ db/migrate/20170323092151_add_required_columns_in_ft_customers.rb
@@ -1,7 +1,7 @@
 class AddRequiredColumnsInFtCustomers < ActiveRecord::Migration
   def change
     add_column :ft_customers, :allow_apbs, :string, :limit => 1, :comment => "the transfer type for aadhar payments"
-    add_column :ft_customers, :abps_user_no, :string, :limit => 50, :comment => "the unique no of the user which will be allocated by NPCI"  
-    add_column :ft_customers, :abps_user_name, :string, :limit => 50, :comment => "the name of the user which will be allocated by NPCI"  
+    add_column :ft_customers, :apbs_user_no, :string, :limit => 50, :comment => "the unique no of the user which will be allocated by NPCI"  
+    add_column :ft_customers, :apbs_user_name, :string, :limit => 50, :comment => "the name of the user which will be allocated by NPCI"  
   end
 end
