7ac3ed9ceb976d69f2f3145b9f8d8168b0177919
divyajayan <divya.jayan@quantiguous.com>
2017-04-03 17:09:29 +0530

    fixed specs

--- db/migrate/20170327121831_add_required_columns_in_funds_transfers.rb
+++ db/migrate/20170327121831_add_required_columns_in_funds_transfers.rb
@@ -1,7 +1,9 @@
 class AddRequiredColumnsInFundsTransfers < ActiveRecord::Migration
   def change
-    add_column :funds_transfers, :aadhar_no, :string, :limit => 12, :comment => "the aadhar number of the beneficiary for apbs transfer type"
-    add_column :funds_transfers, :aadhar_mobile_no, :string, :limit => 20, :comment => "the mobile number which linked with aadhar for APBS transfer type"
-    add_column :funds_transfers, :bene_bank_name, :string, :limit => 50, :comment => "the bene bank name returned from npci for APBS transfer type"  	
+    if ActiveRecord::Base.connection.table_exists? 'funds_transfers'
+      add_column :funds_transfers, :aadhar_no, :string, :limit => 12, :comment => "the aadhar number of the beneficiary for apbs transfer type"
+      add_column :funds_transfers, :aadhar_mobile_no, :string, :limit => 20, :comment => "the mobile number which linked with aadhar for APBS transfer type"
+      add_column :funds_transfers, :bene_bank_name, :string, :limit => 50, :comment => "the bene bank name returned from npci for APBS transfer type"
+    end
   end
 end
