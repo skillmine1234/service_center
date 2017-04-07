aa761d7f51eb9aec592f0a75b828276f6f81c391
divyajayan <divya.jayan@quantiguous.com>
2017-04-07 11:41:04 +0530

    checked funds_transfers table exists condition in migration

--- db/migrate/20170406091820_rename_aadhar_no_aadhar_mobile_no_in_funds_transfers.rb
+++ db/migrate/20170406091820_rename_aadhar_no_aadhar_mobile_no_in_funds_transfers.rb
@@ -1,6 +1,8 @@
 class RenameAadharNoAadharMobileNoInFundsTransfers < ActiveRecord::Migration
   def change
-    rename_column :funds_transfers, :aadhar_no, :aadhaar_no
-    rename_column :funds_transfers, :aadhar_mobile_no, :aadhaar_mobile_no    
+    if ActiveRecord::Base.connection.table_exists? 'funds_transfers'
+      rename_column :funds_transfers, :aadhar_no, :aadhaar_no
+      rename_column :funds_transfers, :aadhar_mobile_no, :aadhaar_mobile_no
+    end
   end
 end
