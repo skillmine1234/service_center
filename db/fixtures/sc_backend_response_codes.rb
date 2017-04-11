70e175faefaee9002858e7c5ac1780820a2833ad
naveenqg <NaveenVunnam@Apples-MacBook-Pro-4.local>
2017-04-11 19:24:42 +0530

    SY-148:created seed for flex error codes

---
+++ db/fixtures/sc_backend_response_codes.rb
@@ -0,0 +1,577 @@
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E9072'
+ s.fault_code = 'flex:E9072'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:EPM0083'
+ s.fault_code = 'flex:E9072'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:EPM0082'
+ s.fault_code = 'flex:E9072'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:EPM0027'
+ s.fault_code = 'flex:E9072'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E8036'
+ s.fault_code = 'flex:E8036'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90288'
+ s.fault_code = 'flex:E8036'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E8087'
+ s.fault_code = 'flex:E8087'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E10011'
+ s.fault_code = 'flex:E8087'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90296'
+ s.fault_code = 'flex:E8087'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E18'
+ s.fault_code = 'flex:E18'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:EEXT_RSP9018'
+ s.fault_code = 'flex:E18' 
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E8037'
+ s.fault_code = 'ns:E402'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:EEXT_RSP9045'
+ s.fault_code = 'ns:E402'
+ s.approval_status = 'A'
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:EEXT_RSP9031'
+ s.fault_code = 'ns:E402'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:EEXT_RSP9086'
+ s.fault_code = 'ns:E402'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E1112'
+ s.fault_code = 'ns:E402'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E6833'
+ s.fault_code = 'flex:E6833'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:EE0010'
+ s.fault_code = 'flex:EE0010'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E9006'
+ s.fault_code = 'flex:E9006'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90073'
+ s.fault_code = 'flex:E9006'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90074'
+ s.fault_code = 'flex:E9006'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90007'
+ s.fault_code = 'flex:E9006'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E2118'
+ s.fault_code = 'flex:E2118'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90076'
+ s.fault_code = 'flex:E2118'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E9026'
+ s.fault_code = 'flex:E9026'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90173'
+ s.fault_code = 'flex:E9026'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11000'
+ s.fault_code = 'flex:E11000'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90223'
+ s.fault_code = 'flex:E11000'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11001'
+ s.fault_code = 'flex:E11001'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90225'
+ s.fault_code = 'flex:E11001'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11004'
+ s.fault_code = 'flex:E11004'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90231'
+ s.fault_code = 'flex:E11004'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11005'
+ s.fault_code = 'flex:E11005'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90233'
+ s.fault_code = 'flex:E11005'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11011'
+ s.fault_code = 'flex:E11011'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90235'
+ s.fault_code = 'flex:E11011'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11012'
+ s.fault_code = 'flex:E11012'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90239'
+ s.fault_code = 'flex:E11012'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11013'
+ s.fault_code = 'flex:E11013'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90222'
+ s.fault_code = 'flex:E11013'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11014'
+ s.fault_code = 'flex:E11014'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90224'
+ s.fault_code = 'flex:E11014'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11015'
+ s.fault_code = 'flex:E11015'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90230'
+ s.fault_code = 'flex:E11015'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E11016'
+ s.fault_code = 'flex:E11016'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E90232'
+ s.fault_code = 'flex:E11016'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'flex:E3573'
+ s.fault_code = 'flex:E6833'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:E70'
+ s.fault_code = 'sfms:E70'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:E18'
+ s.fault_code = 'sfms:E18'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:E99'
+ s.fault_code = 'sfms:E99'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:E28'
+ s.fault_code = 'sfms:E28'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:E30'
+ s.fault_code = 'sfms:E30'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:E93'
+ s.fault_code = 'sfms:E93'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:E62'
+ s.fault_code = 'sfms:E62'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:E19'
+ s.fault_code = 'sfms:E19'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:EFRJ'
+ s.fault_code = 'sfms:EFRJ'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:EPIR'
+ s.fault_code = 'sfms:EPIR'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:ERBA'
+ s.fault_code = 'sfms:ERBA'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:ERBH'
+ s.fault_code = 'sfms:ERBH'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:ERBR'
+ s.fault_code = 'sfms:ERBR'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:EREJ'
+ s.fault_code = 'sfms:EREJ'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:ESSN'
+ s.fault_code = 'sfms:ESSN'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:ETRJ'
+ s.fault_code = 'sfms:ETRJ'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'sfms:EREV'
+ s.fault_code = 'sfms:EREV'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'CODE_NOT_FOUND'
+ s.fault_code = 'ns:E1015'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'CODE_GATEWAY_TIMEOUT'
+ s.fault_code = 'ns:E504'
+ s.approval_status = 'A' 
+end
+
+ScBackendResponseCode.seed_once(:sc_backend_code, :response_code) do |s|
+ s.is_enabled = 'Y'
+ s.sc_backend_code = 'FCR'
+ s.response_code = 'CODE_BAD_GATEWAY'
+ s.fault_code = 'ns:E502'
+ s.approval_status = 'A' 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ db/fixtures/sc_fault_codes.rb
@@ -0,0 +1,354 @@
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'ns:E402'
+ s.fault_reason = 'Insufficient Balance in debit account, payment required'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'Fund the debit account and retry'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E9072'
+ s.fault_reason = 'Destination Bank and Branch could not be resolved'
+ s.fault_kind = 'B'
+ s.occurs_when = 'N'
+ s.remedial_action = 'Retry with correct beneficiary IFSC Code'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E8036'
+ s.fault_reason = 'NEFT - Both Customer Mobile and Email is not valid'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'Issue with Remitter Account setup Contact Support'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E8087'
+ s.fault_reason = 'To Account Number is Invalid'
+ s.fault_kind = 'B'
+ s.occurs_when = 'To Account Number is Invalid'
+ s.remedial_action = 'Retry with correct Beneficiary/customer Account'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E18'
+ s.fault_reason = 'Hold Funds Present - Refer to Drawer ( Account would Overdraw )'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'Account is in hold status. Contact Support'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E6833'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:EE0010'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E9006'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E2118'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E9026'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11000'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11001'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11004'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11005'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11011'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11012'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11013'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11014'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11015'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E11016'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'flex:E6833'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:E70'
+ s.fault_reason = 'Outward Transaction Rejected'
+ s.fault_kind = 'B'
+ s.occurs_when = 'Transaction is in error due to issue with the transaction details.'
+ s.remedial_action = 'Give back money'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:E18'
+ s.fault_reason = 'Rejected by SFMS'
+ s.fault_kind = 'B'
+ s.occurs_when = 'Transaction is not accepted by the payment body'
+ s.remedial_action = 'Give back money'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:E99'
+ s.fault_reason = 'Manually Marked in Error'
+ s.fault_kind = 'B'
+ s.occurs_when = 'Transaction is in error due to issue with the transaction details'
+ s.remedial_action = 'Give back money'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:E28'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:E30'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:E93'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:E62'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'Transaction accepted by RBI but beneficiary bank rejected it'
+ s.remedial_action = 'Give back money'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:E19'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:EFRJ'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:EPIR'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:ERBA'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:ERBH'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:ERBR'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:EREJ'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:ESSN'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:ETRJ'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:EREV'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'SFMS:EREV'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'ns:E1015'
+ s.fault_reason = 'NULL'
+ s.fault_kind = 'B'
+ s.occurs_when = 'NULL'
+ s.remedial_action = 'NULL'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'ns:E504'
+ s.fault_reason = 'Gateway Timeout'
+ s.fault_kind = 'B'
+ s.occurs_when = 'An upstream service times out'
+ s.remedial_action = 'Retry Request After 15 checking the status'
+end
+
+ScFaultCode.seed_once(:fault_code) do |s|
+ s.fault_code = 'ns:E502'
+ s.fault_reason = 'Bad Gateway'
+ s.fault_kind = 'B'
+ s.occurs_when = 'An upstream service returned an unexpected error'
+ s.remedial_action = 'Contact Support'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
