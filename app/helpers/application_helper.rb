3b9d7884c0875a571dc50b07c833deaab9fbd17d
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2017-04-15 18:43:13 +0100

    missing pagination helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,4 +45,12 @@ module ApplicationHelper
     d = Date.strptime(date, '%d-%m-%Y') rescue false
     d != false ? true : false
   end
+
+  def advanced_search_page_links
+    capture do
+      concat link_to_if @records.previous_page, 'Previous', "javascript:$('#page').val(#{@records.previous_page});$('#advanced_search').submit()"
+      concat " "
+      concat link_to_if @records.next_page, 'Next', "javascript:$('#page').val(#{@records.next_page});$('#advanced_search').submit()"
+    end
+  end
 end
