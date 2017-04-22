dc083f598b6fa0ed679e294ef7b53e0bb1efccdd
Akhilesh Kataria <akhilesh.kataria@quantiguous.com>
2017-04-22 13:56:31 +0530

    Update 20170417104501_add_required_columns_to_ecol_customers.rb

--- db/migrate/20170417104501_add_required_columns_to_ecol_customers.rb
+++ db/migrate/20170417104501_add_required_columns_to_ecol_customers.rb
@@ -1,5 +1,5 @@
 class AddRequiredColumnsToEcolCustomers < ActiveRecord::Migration
   def change
-  	 add_column :ecol_customers, :should_prevalidate, :string, :limit => 1, default: 'N', null: false,  :comment => "the identifier to check whether prevalidation is required or not"
+  	 add_column :ecol_customers, :should_prevalidate, :string, :limit => 1, default: 'N', null: false,  :comment => "the identifier to specify if a local prevalidation is to be done before calling the webservice"
   end
 end
