b048ba2444e20de5aa77d357e2b4e25aa120f9e3
divyajayan <divya.jayan@quantiguous.com>
2017-05-02 12:29:20 +0530

    FT2 async changes

--- db/migrate/20170501100830_add_column_allow_async_in_ft_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnAllowAsyncInFtCustomers < ActiveRecord::Migration
-  def change
-    add_column :ft_customers, :allow_async, :string, limit: 1, null: false, default: 'N', comment: 'the flag which indicates whether to allow async or not for the customer'
-  end
-end
