7b57bf6abd1b49f5244141aa23f02c9710efeca0
divyajayan <divya.jayan@quantiguous.com>
2017-05-09 11:08:59 +0530

    added run_every_hour in sc_jobs

--- db/migrate/20170508062550_add_column_run_every_hour_to_sc_jobs.rb
+++ db/migrate/20170508062550_add_column_run_every_hour_to_sc_jobs.rb
@@ -1,5 +1,5 @@
 class AddColumnRunEveryHourToScJobs < ActiveRecord::Migration
   def change
-    add_column :sc_jobs, :run_every_hour, :number, :comment => "the running hours frequency to schedule the report"
+    add_column :sc_jobs, :run_every_hour, :integer, :comment => "the running hours frequency to schedule the report"
   end
 end
