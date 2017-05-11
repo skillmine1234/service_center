fec846d031d565e3162a9c45bf85aef42433782c
naveen <naveen.pokuri@quantiguous>
2017-05-11 14:05:02 +0530

    Renamed columns

--- db/migrate/20170503100810_add_column_use_global_config_to_bm_apps.rb
+++ db/migrate/20170503100810_add_column_use_global_config_to_bm_apps.rb
@@ -1,7 +1,7 @@
 class AddColumnUseGlobalConfigToBmApps < ActiveRecord::Migration
   def change
-    add_column :bm_apps, :use_global_config, :string, :limit => 1, default: 'Y', comment: 'the flag to decide global or pre-app values for narrative and userid'
-    add_column :bm_apps, :user_id, :string, :limit => 50, comment: 'the user id for the fcr request'
-    add_column :bm_apps, :narrative_prefix, :string, :limit => 50, comment: 'the narrative for the fcr request'        
+    add_column :bm_apps, :is_configuration_global, :string, :limit => 1, default: 'Y', comment: 'the flag to decide global or pre-app values for narrative and userid'
+    add_column :bm_apps, :flex_user_id, :string, :limit => 50, comment: 'the flex user id for the fcr request'
+    add_column :bm_apps, :flex_narrative_prefix, :string, :limit => 50, comment: 'the narrative for the fcr request'        
   end
 end
