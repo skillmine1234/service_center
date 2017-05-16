5f2df4e34e88da9bae64f87c44bad57d66a9862a
neha <neha.upadhyay@quantiguous.com>
2017-05-16 16:37:01 +0530

    ft-295: fixed the column length

--- db/migrate/20170510130336_add_columns_to_funds_transfers.rb
+++ db/migrate/20170510130336_add_columns_to_funds_transfers.rb
@@ -1,6 +1,6 @@
 class AddColumnsToFundsTransfers < ActiveRecord::Migration
   if ActiveRecord::Base.connection.table_exists? 'funds_transfers'
-    add_column :funds_transfers, :customer_name, :string, :limit => 255, :comment => "the name of the customer"    
+    add_column :funds_transfers, :customer_name, :string, :limit => 100, :comment => "the name of the customer"    
     add_column :funds_transfers, :account_mmid, :string, :limit => 10, :comment => "the seven digit number of which the first four digits are the unique identification number of the bank offering IMPS"    
     add_column :funds_transfers, :registered_mobile_no, :string, :limit => 15, :comment => "the mobile no of the registered customer for IMPS"  
   end
