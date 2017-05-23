8e1899de6e60aa27627cbe58c706372ba4eda1c1
naveen <naveen.pokuri@quantiguous>
2017-05-23 15:52:57 +0530

    added new column user_id to bm_rules

--- db/migrate/20170520144713_add_column_narrative_prefix_to_bm_rules.rb
+++ db/migrate/20170520144713_add_column_narrative_prefix_to_bm_rules.rb
@@ -1,5 +1,6 @@
 class AddColumnNarrativePrefixToBmRules < ActiveRecord::Migration
   def change
     add_column :bm_rules, :narrative_prefix, :string, :limit => 50, :comment => "the narrative value which will used in fcr request"    
+    add_column :bm_rules, :user_id, :string, :limit => 50, :comment => "the userID of the flex API"    
   end
 end
