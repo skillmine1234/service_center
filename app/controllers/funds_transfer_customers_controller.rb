149f869f8c557cfc7fdbd12a0a5732e7e5304f84
divyajayan <divya.jayan@quantiguous.com>
2017-05-24 23:41:38 +0530

    FT-321 Screen changes in FtCustomer for FT2 Notify

--- app/controllers/funds_transfer_customers_controller.rb
+++ app/controllers/funds_transfer_customers_controller.rb
@@ -100,6 +100,7 @@ class FundsTransferCustomersController < ApplicationController
     params.require(:funds_transfer_customer).permit(:app_id, :name, :low_balance_alert_at, :identity_user_id, :allow_neft, :allow_imps, 
                                                     :enabled, :customer_id, :lock_version, :approval_status, :allow_rtgs, :is_retail,
                                                     :last_action, :approved_version, :approved_id, :created_by, :updated_by, :needs_purpose_code,
-                                                    :reply_with_bene_name, :allow_all_accounts, :is_filetoapi_allowed)
+                                                    :reply_with_bene_name, :allow_all_accounts, :is_filetoapi_allowed, :allow_apbs, :apbs_user_no, 
+                                                    :apbs_user_name, :allow_start_transfer, :notify_on_status_change, :notify_app_code)
   end
 end
