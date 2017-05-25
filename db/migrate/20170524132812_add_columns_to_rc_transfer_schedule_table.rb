166c74a5c7af4fc53cc3310a7979deadbe83a100
naveen <naveen.pokuri@quantiguous>
2017-05-25 19:52:50 +0530

    RTV-33 : added columns to rc_transfers and rc_transfer_schedule and created a new model

--- db/migrate/20170524132812_add_columns_to_rc_transfer_schedule_table.rb
+++ db/migrate/20170524132812_add_columns_to_rc_transfer_schedule_table.rb
@@ -2,7 +2,7 @@ class AddColumnsToRcTransferScheduleTable < ActiveRecord::Migration
   def up
     add_column :rc_transfer_schedule, :ch_sweep_out, :number, :null => false, :default => 0, :comment => "the minimum account balance to be maintained during the transfer"    
     add_column :rc_transfer_schedule, :bene_account_ifsc, :string, :limit => 11, :null => false, :default => 'YESB0000001', :comment => "the beneficiary account IFSC"          
-    add_column :rc_transfer_schedule, :max_retries, :integer, :null => false, :default => 0, :comment => "the retry count for tranfer" 
+    add_column :rc_transfer_schedule, :max_retries, :integer, :null => false, :default => 0, :comment => "the retry count for schedule/instruction" 
     add_column :rc_transfer_schedule, :retry_in_mins, :integer, :null => false, :default => 0, :comment => "the schedule/instruction attempt number"         
   end
 
