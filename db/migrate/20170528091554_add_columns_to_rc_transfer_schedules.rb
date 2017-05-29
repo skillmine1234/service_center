1eb90edfdda2e6e03d5eb4c2e77338ca792e447b
naveen <naveen.pokuri@quantiguous>
2017-05-29 10:05:13 +0530

    RTV-33 : added new column for bene_name

--- db/migrate/20170528091554_add_columns_to_rc_transfer_schedules.rb
+++ db/migrate/20170528091554_add_columns_to_rc_transfer_schedules.rb
@@ -1,5 +1,6 @@
 class AddColumnsToRcTransferSchedules < ActiveRecord::Migration
   def change
     add_column :rc_transfer_schedule, :bene_name, :string, :limit => 25, :comment => "the beneficiaary  name which we used in FCR call" 
+    add_column :rc_transfer_schedule, :rmtr_name, :string, :limit => 25, :comment => "the remitter  name which we used in FCR call"         
   end
 end
