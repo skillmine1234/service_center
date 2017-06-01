aa6698a86402a33c2f8e6d79133cc0df265109b5
Lavanya Ramamoorthy <lavanya.ramamoorthy@quantiguous.com>
2017-06-01 21:48:46 +0100

    RP-75 - fixing autosave in admin login

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -7,4 +7,15 @@ $(document).ready(function(){
   $('head').append('<meta content="no-cache" http-equiv="Cache-control">');
   $('head').append('<meta content="no-store" http-equiv="Cache-control">');
   $(".ui-datepicker-inline").width("75em");
+
+  $('#session_new').submit(function(){
+    $("#admin_user_username").val($('#username-show').val());
+    $("#admin_user_password").val($('#password-show').val());
+  });
+    
+  $("#username-show,#password-show").keypress(function(e) {
+    if (e.which == 13) {
+      $("#session_new").submit();
+    }
+  });
 });
