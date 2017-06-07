7fc18f7b816cd77658f748cd3f4f653a229adca8
Neha1993 <neha.upadhyay@quantiguous.com>
2017-06-07 20:28:11 +0530

    rtv-33: fixed the comment for the columns

--- db/migrate/20170602081506_add_columns_bene_name_to_rc_transfers.rb
+++ db/migrate/20170602081506_add_columns_bene_name_to_rc_transfers.rb
@@ -1,6 +1,6 @@
 class AddColumnsBeneNameToRcTransfers < ActiveRecord::Migration
   def change
-    add_column :rc_transfers, :bene_name, :string, :limit => 25, :comment => "the beneficiaary  name which we used in FCR call" 
-    add_column :rc_transfers, :bene_account_ifsc, :string, :limit => 11, :comment => "the beneficiary account IFSC"     
+    add_column :rc_transfers, :bene_name, :string, :limit => 25, :comment => "the full name of the beneficiary which will be used to call FCR API" 
+    add_column :rc_transfers, :bene_account_ifsc, :string, :limit => 11, :comment => "the ifsc code for the beneficiary account"     
   end
 end
