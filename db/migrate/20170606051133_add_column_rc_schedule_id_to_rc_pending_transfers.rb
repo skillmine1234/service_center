ce57fa3a2b8797ff7a8a23183053bf721bfaae6f
Neha1993 <neha.upadhyay@quantiguous.com>
2017-06-08 17:11:20 +0530

    changes are done as suggested

--- db/migrate/20170606051133_add_column_rc_schedule_id_to_rc_pending_transfers.rb
+++ db/migrate/20170606051133_add_column_rc_schedule_id_to_rc_pending_transfers.rb
@@ -1,5 +1,10 @@
 class AddColumnRcScheduleIdToRcPendingTransfers < ActiveRecord::Migration
   def change
-    add_column :rc_pending_schedules, :rc_schedule_id, :integer, :null => false, :comment => "the id of the row that represents the schedule that is related to this record" 
+    add_column :rc_pending_schedules, :rc_schedule_id, :integer, :comment => "the id of the row that represents the schedule that is related to this record"
+    db.execute "UPDATE rc_pending_schedules SET rc_schedule_id = 0"
+    change_column :rc_pending_schedules, :rc_schedule_id, :integer, :null => false, :comment => "the id of the row that represents the schedule that is related to this record"    
   end
+  def db
+    ActiveRecord::Base.connection
+  end  
 end
