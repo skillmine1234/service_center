a066f5867098f7ac77f3fc88d1744781782e1108
divyajayan <divya.jayan@quantiguous.com>
2017-06-15 21:25:24 +0530

    FT-276 set allowed_transfer_types as APBS for APBS purpose_code

--- app/models/ft_purpose_code.rb
+++ app/models/ft_purpose_code.rb
@@ -14,6 +14,8 @@ class FtPurposeCode < ActiveRecord::Base
 
   belongs_to :created_user, :foreign_key =>'created_by', :class_name => 'User'
   belongs_to :updated_user, :foreign_key =>'updated_by', :class_name => 'User'
+  
+  before_validation :set_transfer_type, unless: "self.frozen?"
 
   validates_presence_of :code, :description, :is_enabled, :allowed_transfer_types
   validates :code, format: {with: /\A[a-z|A-Z|0-9]+\z/, :message => 'Invalid format, expected format is : {[a-z|A-Z|0-9]}'}, length: {minimum: 2, maximum: 6}
@@ -28,6 +30,10 @@ class FtPurposeCode < ActiveRecord::Base
   before_save :set_settings_cnt, unless: "self.frozen?"
 
   private
+  
+  def set_transfer_type
+    self.allowed_transfer_types = ['APBS'] if (self.approved_record.present? && self.approved_record.allowed_transfer_types == ['APBS'] && allowed_transfer_types.blank?)
+  end
 
   def value_for_reg_bene_for_apbs
     errors.add(:allow_only_registered_bene, "Allow Only Registered Bene cannot be enabled when APBS is allowed") if allow_only_registered_bene == 'Y'
