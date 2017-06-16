638cdb5be5c28f3e07048fa03a794faec1364405
divyajayan <divya.jayan@quantiguous.com>
2017-06-16 12:48:58 +0530

    FT-276 description should be editable for frozen record

--- app/assets/javascripts/ft_purpose_code.js
+++ app/assets/javascripts/ft_purpose_code.js
@@ -20,7 +20,6 @@ $(document).ready(function(){
 
   if($('#ft_purpose_code_is_frozen').val() == 'Y'){
     $('#ft_purpose_code_code').prop('readonly',true);
-    $('#ft_purpose_code_description').prop('readonly',true);
     $('#ft_purpose_code_allow_only_registered_bene').prop('disabled',true);
     $('#ft_purpose_code_allowed_transfer_types').prop('disabled',true);
   }
