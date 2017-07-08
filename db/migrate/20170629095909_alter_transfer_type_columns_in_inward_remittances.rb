d7561bd791bc8b787706a7fbb9ba01af65ebbf6b
neha <neha.upadhyay@quantiguous.com>
2017-07-08 15:01:45 +0530

    SY-255 - partners table - comments corrected for changed columns

--- db/migrate/20170629095909_alter_transfer_type_columns_in_inward_remittances.rb
+++ db/migrate/20170629095909_alter_transfer_type_columns_in_inward_remittances.rb
@@ -1,6 +1,6 @@
 class AlterTransferTypeColumnsInInwardRemittances < ActiveRecord::Migration
   def change
-    change_column :inward_remittances, :transfer_type, :string, limit: 10, :comment => "the transfer type of the transactions whether e.g. NEFT,FT,IMPS,RTGS,THRIFTY,ANY"  
-    change_column :inward_remittances, :req_transfer_type, :string, limit: 10, :comment => "the transfer type of the transactions whether e.g. NEFT,FT,IMPS,RTGS,THRIFTY,ANY"
+    change_column :inward_remittances, :transfer_type, :string, limit: 10, :comment => "the type of the transfer which has been used for transactions (e.g. NEFT,FT,IMPS,RTGS)"  
+    change_column :inward_remittances, :req_transfer_type, :string, limit: 10, :comment => "the requested transfer type, can be different from used in transfer_type (e.g. NEFT,FT,IMPS,RTGS,THRIFTY,ANY)"
   end
 end
--- db/migrate/20170629105052_add_auto_reschdl_to_next_wrk_day_to_partners.rb
+++ db/migrate/20170629105052_add_auto_reschdl_to_next_wrk_day_to_partners.rb
@@ -1,5 +1,5 @@
 class AddAutoReschdlToNextWrkDayToPartners < ActiveRecord::Migration
   def change
-    add_column :partners, :auto_reschdl_to_next_wrk_day, :string, :limit => 1, :default => "Y", :comment => "the indetifier to specify if the transaction has to be rescheduled for the next working day intead of failing it."  
+    add_column :partners, :auto_reschdl_to_next_wrk_day, :string, :limit => 1, :default => "Y", :comment => "the identifier to specify if the transaction has to be rescheduled for the next working day instead of failing it."  
   end
 end
