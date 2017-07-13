9a4959e7d228d0ca83ba154913e817b9f1d1c446
neha <neha.upadhyay@quantiguous.com>
2017-07-13 15:30:02 +0530

    sy-255: changes are done as suggestd

--- db/migrate/20170713091512_change_column_value_in_partners.rb
+++ db/migrate/20170713091512_change_column_value_in_partners.rb
@@ -1,8 +1,5 @@
 class ChangeColumnValueInPartners < ActiveRecord::Migration
   def change
-    db.execute "update partners set auto_reschdl_to_next_wrk_day = 'N' where service_name = 'INW'"
-  end
-  def db
-    ActiveRecord::Base.connection
-  end  
+    Partner.where("service_name =?","INW").update_all(auto_reschdl_to_next_wrk_day: 'N')
+  end 
 end
