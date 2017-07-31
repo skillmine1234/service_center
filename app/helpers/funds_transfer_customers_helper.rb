72bfe44ae60e05d9f3e63df9de1df720af732df3
divyajayan <divya.jayan@quantiguous.com>
2017-08-01 00:39:33 +0530

    SY-271 fix in show page

--- app/helpers/funds_transfer_customers_helper.rb
+++ app/helpers/funds_transfer_customers_helper.rb
@@ -5,4 +5,16 @@ module FundsTransferCustomersHelper
     funds_transfer_customers = funds_transfer_customers.where("app_id=?",params[:app_id]) if params[:app_id].present?
     funds_transfer_customers
   end
+  
+  def get_allowed_relns(ft_customer)
+    if ft_customer.allowed_relns.empty?
+      if ft_customer.is_retail == 'Y'
+        return "GUR,JOF,JOO,SOW,TRU" 
+      else
+        return "GUR,JOF,JOO,SOW,TRU,AUS"
+      end
+    else
+      return ft_customer.allowed_relns.join(',')
+    end
+  end
 end
