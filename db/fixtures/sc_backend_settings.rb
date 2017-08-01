de12ec6c3ff0c06dbc5ca4fed543945fc9713fdb
Neha1993 <neha.upadhyay@quantiguous.com>
2017-08-01 10:41:03 +0530

    SY-164: added seed data for channel

--- db/fixtures/sc_backend_settings.rb
+++ db/fixtures/sc_backend_settings.rb
@@ -2,22 +2,9 @@ ScBackendSetting.seed_once(:backend_code, :service_code, :app_id) do |s|
   s.backend_code = 'FCR'
   s.service_code = 'FT'
   s.app_id = nil
-  s.setting1_name = 'SessionContext.UserID'
+  s.setting1_name = 'userId'
   s.setting1_type = 'text'
-  s.setting1_value = 'vendor'
-
-  s.created_by = 'Q'
-  s.created_at = Time.zone.now
-  s.approval_status = 'A'
-end
-
-ScBackendSetting.seed_once(:backend_code, :service_code, :app_id) do |s|
-  s.backend_code = 'FCR'
-  s.service_code = 'BM'
-  s.app_id = nil
-  s.setting1_name = 'UserID'
-  s.setting1_type = 'text'
-  s.setting1_value = 'username'
+  s.setting1_value = 'EXCHUSER'
 
   s.created_by = 'Q'
   s.created_at = Time.zone.now
@@ -28,12 +15,12 @@ ScBackendSetting.seed_once(:backend_code, :service_code, :app_id) do |s|
   s.backend_code = 'ATOM'
   s.service_code = 'FT'
   s.app_id = nil
-  s.setting1_name = 'UserID'
+  s.setting1_name = 'vendorId'
   s.setting1_type = 'text'
-  s.setting1_value = 'username'
-  s.setting2_name = 'UserID'
+  s.setting1_value = 'ESBU0010'
+  s.setting2_name = 'vendorPassword'
   s.setting2_type = 'text'
-  s.setting2_value = 'username'
+  s.setting2_value = '6d4946909'
 
   s.created_by = 'Q'
   s.created_at = Time.zone.now
