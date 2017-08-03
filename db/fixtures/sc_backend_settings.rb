ab13e88a4db30c424211db1e2b29a45e9d4ad4a6
Neha1993 <neha.upadhyay@quantiguous.com>
2017-08-03 11:56:43 +0530

    added the default value for BPS service

--- db/fixtures/sc_backend_settings.rb
+++ db/fixtures/sc_backend_settings.rb
@@ -33,10 +33,10 @@ ScBackendSetting.seed_once(:backend_code, :service_code, :app_id) do |s|
   s.app_id = nil
   s.setting1_name = 'userId'
   s.setting1_type = 'text'
-  s.setting1_value = nil
+  s.setting1_value = 'MOBUSER'
   s.setting2_name = 'narrativePrefix'
   s.setting2_type = 'text'
-  s.setting2_value = nil
+  s.setting2_value = 'MOBTXN'
   s.created_by = 'Q'
   s.created_at = Time.zone.now
   s.approval_status = 'A'
