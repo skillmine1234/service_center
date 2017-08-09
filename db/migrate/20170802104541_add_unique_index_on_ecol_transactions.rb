82d859022a24234cd5fddb0a3cbca76469efec31
naveen <naveen.pokuri@quantiguous>
2017-08-09 10:12:37 +0530

    SY-279: changes as per review comments

--- db/migrate/20170802104541_add_unique_index_on_ecol_transactions.rb
+++ db/migrate/20170802104541_add_unique_index_on_ecol_transactions.rb
@@ -1,9 +1,9 @@
 class AddUniqueIndexOnEcolTransactions < ActiveRecord::Migration
   def change
      if Rails.configuration.database_configuration[Rails.env]["adapter"] == 'oracle_enhanced'
-       execute "DROP INDEX ECOL_TRANSACTION_UNIQUE_INDEX "
-       execute "CREATE UNIQUE INDEX ECOL_TRANSACTION_UNIQUE_INDEX ON ECOL_TRANSACTIONS (TRANSFER_UNIQUE_NO,TRANSFER_TYPE,RMTR_ACCOUNT_IFSC) "
-       execute "CREATE UNIQUE INDEX ECOL_TRANSACTION_UNIQUE_1 ON ECOL_TRANSACTIONS(CASE WHEN TRANSFER_TYPE !='NEFT' THEN TRANSFER_UNIQUE_NO END,CASE WHEN TRANSFER_TYPE !='NEFT' THEN TRANSFER_TYPE END) "                     
+       remove_index :ecol_transactions, name: 'ecol_transaction_unique_index'
+       add_index :ecol_transactions, [:transfer_unique_no, :transfer_type, :rmtr_account_ifsc], name: 'ecol_transactions_03'
+       execute "CREATE UNIQUE INDEX ECOL_TRANSACTIONS_04 ON ECOL_TRANSACTIONS(CASE WHEN TRANSFER_TYPE !='NEFT' THEN TRANSFER_UNIQUE_NO END,CASE WHEN TRANSFER_TYPE !='NEFT' THEN TRANSFER_TYPE END) "                     
      end
   end
 end
