d0abddd0ce31a19b973aa9ad5c63baf7ac4d445c
divyajayan <divya.jayan@quantiguous.com>
2017-08-18 14:42:27 +0530

    EC-628 removed condition on txn_kind

--- app/searcher/rc_transfer_searcher.rb
+++ app/searcher/rc_transfer_searcher.rb
@@ -24,7 +24,7 @@ class RcTransferSearcher
 
   def find
     reln = RcTransfer.order("id desc")
-    reln = reln.where.not("txn_kind = 'BALINQ' and status_code IN ('BALINQ FAILED','SKIP CREDIT:NO BALANCE')") unless remove_defaults.present?
+    reln = reln.where.not("status_code IN ('BALINQ FAILED','SKIP CREDIT:NO BALANCE')") unless remove_defaults.present?
     reln = reln.where("rc_transfer_code=?", rc_code) if rc_code.present?
     reln = reln.where("bene_account_no=?", bene_account_no) if bene_account_no.present?
     reln = reln.where("debit_account_no=?", debit_account_no) if debit_account_no.present?
