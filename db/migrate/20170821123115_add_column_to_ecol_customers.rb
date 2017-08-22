156a1179395516ca0fa029b0ee004eab435d0423
divyajayan <divya.jayan@quantiguous.com>
2017-08-22 17:18:39 +0530

    SY-274 removed duplicate migration file

--- db/migrate/20170821123115_add_column_to_ecol_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToEcolCustomers < ActiveRecord::Migration
-  def change
-    add_column :ecol_customers, :allowed_operations, :string, comment: 'the field to tell which operations are allowed to customer. For e.g. getstatus, acceptPayment, acceptPaymentWithCreditAcctNo, returnPayment'    
-  end
-end
--- db/migrate/20170822085642_add_column_allowed_operations_to_ecol_customers.rb
+++ db/migrate/20170822085642_add_column_allowed_operations_to_ecol_customers.rb
@@ -1,5 +1,5 @@
 class AddColumnAllowedOperationsToEcolCustomers < ActiveRecord::Migration
   def change
-    add_column :ecol_customers, :allowed_operations, :string, limit: 100, comment: 'the allowed operations for the ecol_customer'
+    add_column :ecol_customers, :allowed_operations, :string, limit: 100, comment: 'the field to tell which operations are allowed to customer. For e.g. getstatus, acceptPayment, acceptPaymentWithCreditAcctNo, returnPayment'
   end
 end
