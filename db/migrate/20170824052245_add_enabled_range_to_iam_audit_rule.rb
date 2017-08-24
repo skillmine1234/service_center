bfd3a23d635d62a683b6a55f1ee7bce84cd382c5
Neha1993 <neha.upadhyay@quantiguous.com>
2017-08-24 14:42:02 +0530

    SY-65: IAM audit rule column change

--- db/migrate/20170824052245_add_enabled_range_to_iam_audit_rule.rb
+++ db/migrate/20170824052245_add_enabled_range_to_iam_audit_rule.rb
@@ -2,6 +2,9 @@ class AddEnabledRangeToIamAuditRule < ActiveRecord::Migration
   def change
     remove_column :iam_audit_rules, :cert_dn
     remove_column :iam_audit_rules, :source_ip
+    remove_column :iam_audit_rules, :org_uuid
+    
+    add_column :iam_audit_rules, :org_uuid, :string, limit: 255, comment: 'the UUID of the organisation as available in DP'
     
     add_column :iam_audit_rules, :log_bad_org_uuid, :string, null: false, default: 'N', limit: 1, comment: 'the timestamp when the log was last enabled'
     add_column :iam_audit_rules, :iam_organisation_id, :int, comment: 'the organisation for which the log is enabled (only 1 org is enabled at a time)'
--- db/migrate/20170824073545_change_column_org_uuid_in_iam_audit_rules.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnOrgUuidInIamAuditRules < ActiveRecord::Migration
-  def change
-    change_column :iam_audit_rules, :org_uuid, :string, limit: 100
-  end
-end
