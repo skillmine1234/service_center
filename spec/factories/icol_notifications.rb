bb672d4a411fdee9b2bd74d8ba98ccdae74ce91c
pawan bora <pawansinghbora4@gmail.com>
2017-09-05 03:54:46 +0530

    ICL-1 spec related changes

--- spec/factories/icol_notifications.rb
+++ spec/factories/icol_notifications.rb
@@ -5,5 +5,13 @@ FactoryGirl.define do
     app_code 'APP12'
     sequence(:customer_code) {|n| "%03i" % "#{n}"}
     status_code 'Success'
+    company_name 'HOODA'
+    sequence(:txn_number) {|n| n}
+    txn_mode 'NEFT'
+    txn_date Time.now
+    payment_status "PASS"
+    template_data "Template"
+    company_id 1
+    template_id 1
   end
 end
