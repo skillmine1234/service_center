f56b8731ec8fee69c14f7f08f0cadc1cc6ffbbcc
pawan bora <pawansinghbora4@gmail.com>
2017-09-05 13:18:15 +0530

    Icl-1 specs for icol_notify_transaction

--- spec/factories/icol_notify_transaction.rb
+++ spec/factories/icol_notify_transaction.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     template_id 1
     compny_id 1
     comapny_name  "hooda"
-    trnsctn_mode "neft"
-    trnsctn_nmbr 1
+    trnsctn_mode "nef"
+    sequence(:trnsctn_nmbr) { |n| n}
     crtd_date_time "2017-09-05 02:23:35"
     payment_status "pas"
     template_data "Template Data"
