352ef7382151e5623fa704174380edb0760f2cce
pawan bora <pawansinghbora4@gmail.com>
2017-09-16 15:33:22 +0530

    ICL-18 template_id nullable

--- db/migrate/20170916080845_column_related_changes_in_icol_notifications.rb
+++ db/migrate/20170916080845_column_related_changes_in_icol_notifications.rb
@@ -2,6 +2,7 @@ class ColumnRelatedChangesInIcolNotifications < ActiveRecord::Migration
   def change
     change_column :icol_notifications, :app_code, :string, limit: 50, :null => true
     change_column :icol_notifications, :company_id, :integer, :null => true
+    change_column :icol_notifications, :template_id, :integer, :null => true
     add_column    :icol_notifications, :updated_at, :datetime, :null => true, comment: "the record is updated at"
   end
 end
