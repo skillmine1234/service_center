a4cd346eaeeb6f8be16b4b7b1116cfba93be8c1b
divyajayan <divya.jayan@quantiguous.com>
2017-09-17 12:49:46 +0530

    ICL-21 added link to IcolCustomer page

--- app/models/icol_notification.rb
+++ app/models/icol_notification.rb
@@ -1,4 +1,5 @@
 class IcolNotification < ActiveRecord::Base
   lazy_load :fault_bitstream
   has_many :icol_notify_steps
+  belongs_to :icol_customer, foreign_key: 'customer_code', primary_key: 'customer_code', class_name: 'IcolCustomer'
 end
--- spec/models/icol_validate_step_spec.rb
+++ spec/models/icol_validate_step_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
-describe IcolNotification do
+describe IcolValidateStep do
   context 'association' do
-    it { should have_many(:icol_notify_steps) }
     it { should belong_to(:icol_customer) }
   end
 end
