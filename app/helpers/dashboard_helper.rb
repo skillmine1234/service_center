476f4f342522e60daa7a8155b7b5485c7f89fb56
divyajayan <divya.jayan@quantiguous.com>
2017-09-27 11:23:48 +0530

    changes in dashboard_helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -12,8 +12,10 @@ module DashboardHelper
       end
       
       items.each do |i|
-        m[:items] << {name: (I18n.t "menu.#{i}"), link_to: polymorphic_url(i.to_s.pluralize.classify.constantize), image_tag: "#{engine.const_get('GROUP')}/#{i}.png", size: '80x80'}
+        m[:items] << {name: (I18n.t "menu.#{i}"), link_to: polymorphic_url(i.to_s.pluralize.classify.constantize), image_tag: "#{gem_name}/#{i}.png", size: '80x80'}
       end
+      
+      m[:common_items] = engine.const_get('COMMON_MENU_ITEMS') rescue []
 
       return m
     end
