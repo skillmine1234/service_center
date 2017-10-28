ce7c08aba881f834553a759a3767b1b01510d5db
divyajayan <divya.jayan@quantiguous.com>
2017-10-28 17:35:14 +0530

    SY-312 fixed error message for setitngs

--- app/models/concerns/setting.rb
+++ app/models/concerns/setting.rb
@@ -71,12 +71,12 @@ module Setting
     end
 
     def validate_setting(attr_name, setting_name, setting_type, setting_value)
-      errors.add(attr_name, "can't be blank") if setting_name.present? && setting_value.blank?
+      errors.add(setting_name, "can't be blank") if setting_name.present? && setting_value.blank?
       if setting_name.present? and setting_value.present?
-        errors.add(attr_name, "invalid format, the correct format is yyyy-mm-dd") if (setting_type == "date" && setting_value =~ /\A\d{4}\-\d{2}\-\d{2}+$\z/).nil?
-        Time.zone.parse setting_value rescue errors.add(attr_name, "is not a date") if setting_type == "date"
-        errors.add(attr_name, "is longer than maximum (100)") if setting_type == "text" && setting_value.length > 100
-        errors.add(attr_name, "should include only digits") if setting_type == "number" && (setting_value =~ /\A[0-9]+$\z/).nil?
+        errors.add(setting_name, "invalid format, the correct format is yyyy-mm-dd") if (setting_type == "date" && setting_value =~ /\A\d{4}\-\d{2}\-\d{2}+$\z/).nil?
+        Time.zone.parse setting_value rescue errors.add(setting_name, "is not a date") if setting_type == "date"
+        errors.add(setting_name, "is longer than maximum (100)") if setting_type == "text" && setting_value.length > 100
+        errors.add(setting_name, "should include only digits") if setting_type == "number" && (setting_value =~ /\A[0-9]+$\z/).nil?
       end
     end
 
