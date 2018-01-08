4a284d2c46ec3f7433dcdbe816b5618e5669e0d2
divyajayan <divya.jayan@quantiguous.com>
2018-01-08 11:37:29 +0530

    SY-318 fixed failing spec

--- spec/factories/output_files.rb
+++ spec/factories/output_files.rb
@@ -4,6 +4,8 @@ FactoryGirl.define do
   factory :fm_output_file do
     sequence(:file_name) { |n| "file#{n}" }
     incoming_file_id { Factory(:incoming_file, approval_status: 'A').id }
+    step_name 'UPLOAD'
+    status_code 'COMPLETED'
     started_at "2015-06-15"
     ended_at "2015-06-15"
   end
