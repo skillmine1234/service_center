cc9b00c5af41b5bebadcc393c28501f512dd60a0
pravinkarale <pravinkarale21@gmail.com>
2020-05-14 17:10:06 +0530

    funds transfer,reverse proxy advanced search issue fix

--- app/helpers/ft_purpose_code_helper.rb
+++ app/helpers/ft_purpose_code_helper.rb
@@ -1,6 +1,6 @@
 module FtPurposeCodeHelper
   def find_ft_purpose_codes(params)
-    ft_purpose_codes = FtPurposeCode
+    ft_purpose_codes = (params[:approval_status].present? and params[:approval_status] == 'U') ? FtPurposeCode.unscoped : FtPurposeCode
     ft_purpose_codes = ft_purpose_codes.where("code IN (?)",params[:code].split(',').collect(&:strip)) if params[:code].present?
     ft_purpose_codes = ft_purpose_codes.where("is_enabled=?",params[:enabled]) if params[:enabled].present?
     ft_purpose_codes
