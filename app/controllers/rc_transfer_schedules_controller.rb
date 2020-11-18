43effef10ea5812a560e3c22ce4088c064bc5baf
Durgasree Boddu <durgasree.boddu@capgemini.com>
2020-11-18 18:16:41 +0530

    rbi chnages as per prod

--- app/controllers/rc_transfer_schedules_controller.rb
+++ app/controllers/rc_transfer_schedules_controller.rb
@@ -118,6 +118,10 @@ class RcTransferSchedulesController < ApplicationController
 
        Net::HTTP.start(uri.host,uri.port,:use_ssl => uri.scheme == 'https',:verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
        request = Net::HTTP::Post.new(uri.request_uri,headers)
+
+       request.ssl_version = :TLSv1 if ENV['SHAKEHAND'] == "true"
+       request.ciphers = ['DES-CBC3-SHA'] if ENV['SHAKEHAND'] == "true"
+
        request_data = {
                         "GetCASADetailsExtReq":
                         {
