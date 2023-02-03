1f8ae429f579dd5ea4f3974f3fadcabadce0faa2
Mahesh Sharma <pravinkarale21@gmail.com>
2023-02-03 12:45:25 +0530

    added

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,7 +5,7 @@ class AdminUser < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   if ENV['DEVISE_AUTHENTICATE_WITH_LDAP'] == "true"
     devise :ldap_authenticatable, :trackable
-    #before_create :get_ldap_name
+    before_create :get_ldap_name
   else
   devise :database_authenticatable, :password_expirable,
          :rememberable, :trackable, :validatable, :timeoutable, :session_limitable
@@ -20,4 +20,19 @@ class AdminUser < ActiveRecord::Base
     username
   end
 
+  def get_ldap_name
+    self.last_name = Devise::LdapAdapter.get_ldap_param(self.username, 'sn') rescue nil
+    self.email = Devise::LdapAdapter.get_ldap_param(self.username, 'mail') rescue "#{username}@ratnakarbank.in"
+    self.mobile_no = Devise::LdapAdapter.get_ldap_param(self.username, 'mobile') rescue nil
+    if self.last_name.blank?
+      errors.add(:username, "Invalid User Id")
+      return false
+    elsif self.first_name.blank?
+      self.first_name = Devise::LdapAdapter.get_ldap_param(self.username, 'givenname') rescue nil
+      errors.add(:username, "User Name: #{self.first_name} #{self.last_name}, Kindly validate name before creation.")
+      return false
+    end
+    return true
+  end
+  
 end
