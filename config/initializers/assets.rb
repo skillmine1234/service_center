8b6d9af0809058f88ae664858aca5b6046e27661
skillmine1234 <rahul.khoragade@skill-mine.com>
2023-09-13 16:55:24 +0530

    new changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.precompile += %w( 123.js )
 Rails.application.config.assets.precompile += %w( application.js )
 Rails.application.config.assets.precompile += %w( inw/*.png)
 Rails.application.config.assets.precompile += %w( ft/*.png )
-#Rails.application.config.assets.precompile += %w( sc/*.png )
+Rails.application.config.assets.precompile += %w( sc/*.png )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
